8727cf31b7f2aa485549d62621021e1137aa2dc9
Daniel Thomas <drt24@cam.ac.uk>
2012-07-02 14:11:20 +0100

    Add puppetlabs-ntp
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

---
+++ manifests/globals.pp
@@ -0,0 +1,6 @@
+$ntp_servers = [ 'server ntp2.csx.cam.ac.uk',
+                 'server ntp1.csx.cam.ac.uk',
+                 'server ntp1.retrosnub.co.uk',
+                 'server ntp1a.cl.cam.ac.uk',
+                 'server ntp1b.cl.cam.ac.uk',
+                 'server ntp1c.cl.cam.ac.uk', ]
