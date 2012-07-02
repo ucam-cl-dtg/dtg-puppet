a321204b0a044cc66574b50bb17e948a98ff68a5
Daniel Thomas <drt24@cam.ac.uk>
2012-07-02 15:41:47 +0100

    munge gpg.pp to not be mayfirst specific
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- modules/gpg/manifests/gpg.pp
+++ modules/gpg/manifests/gpg.pp
@@ -1,18 +1,18 @@
-class m_gpg {
+class gpg {
 
   package{'gnupg':
     ensure => installed,
   }
 
-  file { "/usr/local/sbin/mf-gpg-root-needs-expiration-extended":
-      source => "puppet:///modules/mayfirst/gpg/mf-gpg-root-needs-expiration-extended",
+  file { "/usr/local/sbin/gpg-root-needs-expiration-extended":
+      source => "puppet:///modules/gpg/gpg-root-needs-expiration-extended",
       owner => root,
       group => root,
       mode => 0755
   }
 
-  file { "/usr/local/sbin/mf-gpg-extend-root-expiration":
-      source => "puppet:///modules/mayfirst/gpg/mf-gpg-extend-root-expiration",
+  file { "/usr/local/sbin/gpg-extend-root-expiration":
+      source => "puppet:///modules/gpg/gpg-extend-root-expiration",
       owner => root,
       group => root,
       mode => 0755
@@ -23,12 +23,12 @@ class m_gpg {
 
 # Ensure user has a gpg private key. Typically this define should be used to ensure 
 # that the user has a password-less gpg key to be used in scripts
-define m_gpg::private_key( $homedir, $passphrase = '', $expire = '31536000', $length = 2048, $uid = '' ) {
+define gpg::private_key( $homedir, $passphrase = '', $expire = '31536000', $length = 2048, $uid = '' ) {
 
   $user = $title
 
   $calculated_uid = $uid ? {
-    '' => "$user@$::hostname.mayfirst.org",
+    '' => "$user@$::fqdn",
     default => $uid
   }
 
@@ -40,24 +40,24 @@ define m_gpg::private_key( $homedir, $passphrase = '', $expire = '31536000', $le
       unless => "test -f '$homedir/.ssh/id_rsa'"
   }
   exec { "gpg-pem2openpgp-$user":
-      command => "pem2openpgp $user@$::hostname.mayfirst.org < '$homedir/.ssh/id_rsa' | gpg --import",
+      command => "pem2openpgp $user@$::fqdn < '$homedir/.ssh/id_rsa' | gpg --import",
       environment => [ "HOME=$homedir", "PEM2OPENPGP_EXPIRATION=$expire" ],
       require =>  [ Package["monkeysphere"], Package["openssh-client"], Exec["gpg-ssh-genkey-$user" ] ],
       user => $user,
       unless => "gpg --list-secret-key '$calculated_uid' >/dev/null"
   }
   exec { "gpg-extend-expiration-$user":
-      command => "/usr/local/sbin/mf-gpg-extend-root-expiration",
+      command => "/usr/local/sbin/gpg-extend-root-expiration",
       environment => [ "HOME=$homedir"  ],
-      require =>  [ File["/usr/local/sbin/mf-gpg-root-needs-expiration-extended"], File["/usr/local/sbin/mf-gpg-extend-root-expiration"], Exec["gpg-ssh-genkey-$user" ], Exec["gpg-pem2openpgp-$user"] ],
+      require =>  [ File["/usr/local/sbin/gpg-root-needs-expiration-extended"], File["/usr/local/sbin/gpg-extend-root-expiration"], Exec["gpg-ssh-genkey-$user" ], Exec["gpg-pem2openpgp-$user"] ],
       user => $user,
-      onlyif => "/usr/local/sbin/mf-gpg-root-needs-expiration-extended"
+      onlyif => "/usr/local/sbin/gpg-root-needs-expiration-extended"
   }
   case $passphrase {
     '': { }
     default: { 
       exec { "gpg-add-passphrase-$user":
-        command => "printf '$passphrase\n$passphrase\nsave\n' |gpg --command-fd 0 --passphrase-fd 0 --no-tty --edit-key root@$::hostname.mayfirst.org password",
+        command => "printf '$passphrase\n$passphrase\nsave\n' |gpg --command-fd 0 --passphrase-fd 0 --no-tty --edit-key root@$::fqdn password",
         require =>  [ Exec["gpg-pem2openpgp-$user"] ],
         environment => "HOME=$homedir",
         user => $user,
@@ -68,7 +68,7 @@ define m_gpg::private_key( $homedir, $passphrase = '', $expire = '31536000', $le
 }
 
 # publish key to a keysrver
-define m_gpg::publish_user_key ( $keyserver = '' ){
+define gpg::publish_user_key ( $keyserver = '' ){
   $user = $title
 
   $keyserver_arg = $keyserver ? {
