b32c397546868f570a26cbfd0f3387ef0bfa1800
Daniel Thomas <drt24@cam.ac.uk>
2012-07-11 15:29:21 +0100

    Add intial bootstrap.sh for bootstraping a node - only does git repo setup atm
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

---
+++ modules/dtg/files/bootstrap.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Install puppet
+echo "Installing puppet-common and git-core"
+apt-get -y install puppet-common git-core
+
+# From mfpl-puppet/modules/mayfirst/files/freepuppet/frepuppet-init
+# prep a server to be free of a puppet master
+# re-run this command to clear out your local git
+# repo (allowing you to re-push after git commit --amend)
+
+echo "Setting up git repositories"
+# remove /etc/puppet as installed by puppet
+rm -rf /etc/puppet
+
+mkdir /etc/puppet
+cd /etc/puppet
+git init --shared=group
+
+# create bare repo that admins will push to
+admin_bare_repo="/etc/puppet-bare"
+[ -d "$admin_bare_repo" ] && rm -rf "$admin_bare_repo"
+mkdir "$admin_bare_repo"
+cd "$admin_bare_repo"
+git init --bare --shared=group
+
+# create post-update hook to pull in changes to the real puppet repo
+target="$admin_bare_repo/hooks/post-update"
+printf "echo ---- Pulling changes into /etc/puppet -----\n\n" > "$target"
+printf "cd /etc/puppet\n" >> "$target"
+printf "unset GIT_DIR\n" >> "$target"
+printf "git pull bare  master\n\n" >> "$target"
+printf "echo ---- Applying new recipes ----\n\n" >> "$target"
+printf "/usr/local/sbin/freepuppet-run" >> "$target"
+
+chmod 775 hooks/post-update
+
+# add  as a remote to real puppet repo
+cd /etc/puppet
+git remote add bare "$admin_bare_repo"
+
+echo "Git repositories configured."
