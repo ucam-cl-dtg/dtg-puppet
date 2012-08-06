b60b869827e9e1ddafa58333fd28391522c53143
Daniel Thomas <drt24@cam.ac.uk>
2012-08-06 13:11:30 +0100

    Add missing monkeysphere config to globals.pp
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- manifests/globals.pp
+++ manifests/globals.pp
@@ -4,3 +4,9 @@ $ntp_servers = [ 'server ntp2.csx.cam.ac.uk',
                  'server ntp1a.cl.cam.ac.uk',
                  'server ntp1b.cl.cam.ac.uk',
                  'server ntp1c.cl.cam.ac.uk', ]
+# Admin user ids to be given root on the nodes via monkeysphere
+$ms_admin_user_ids = [
+  "Daniel Thomas <drt24@cam.ac.uk>"
+]
+# Keyserver with the public keys to use for monkeysphere
+$ms_keyserver = 'keys.gnupg.net'
