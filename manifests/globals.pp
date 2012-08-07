a1eaa193ff393d48e1e107e84c63ceb977fc0352
Daniel Thomas <drt24@cam.ac.uk>
2012-08-07 15:17:03 +0100

    in ntp_servers don't need to have the server bit first, doing so results in 'serer server ntp...' which fails rather than 'server ntp...'
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- manifests/globals.pp
+++ manifests/globals.pp
@@ -1,10 +1,10 @@
 Exec { path => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin' }
-$ntp_servers = [ 'server ntp2.csx.cam.ac.uk',
-                 'server ntp1.csx.cam.ac.uk',
-                 'server ntp1.retrosnub.co.uk',
-                 'server ntp1a.cl.cam.ac.uk',
-                 'server ntp1b.cl.cam.ac.uk',
-                 'server ntp1c.cl.cam.ac.uk', ]
+$ntp_servers = [ 'ntp2.csx.cam.ac.uk',
+                 'ntp1.csx.cam.ac.uk',
+                 'ntp1.retrosnub.co.uk',
+                 'ntp1a.cl.cam.ac.uk',
+                 'ntp1b.cl.cam.ac.uk',
+                 'ntp1c.cl.cam.ac.uk', ]
 # Admin user ids to be given root on the nodes via monkeysphere
 $ms_admin_user_ids = [
   "Daniel Thomas <drt24@cam.ac.uk>"
