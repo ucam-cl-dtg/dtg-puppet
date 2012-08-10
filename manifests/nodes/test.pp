1f488684bd459e2e87138e6af3d6b271307a86cb
Daniel Thomas <drt24@cam.ac.uk>
2012-08-10 15:39:40 +0100

    Fill in nagios_monitor spec for test-puppet
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- manifests/nodes/test.pp
+++ manifests/nodes/test.pp
@@ -2,5 +2,9 @@ node 'test-puppet.dtg.cl.cam.ac.uk' {
   include minimal
 }
 if ( $::fqdn == $::nagios_server ) {
-  nagios_monitor { 'test-puppet': }
+  nagios_monitor { 'test-puppet':
+    parents    => '',
+    address    => 'test-puppet.dtg.cl.cam.ac.uk',
+    hostgroups => [ 'ssh-servers'],
+  }
 }
