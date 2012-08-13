4f10b3c98f7c7a0f36811dafed307ee61378ddd2
Daniel Thomas <drt24@cam.ac.uk>
2012-08-13 14:31:23 +0100

    In exim module apply updated configuration and ensure correct mailx is used
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- modules/exim/manifests/init.pp
+++ modules/exim/manifests/init.pp
@@ -10,9 +10,24 @@ class exim::satellite ($smarthost, $mail_domain) {
     content => template('exim/update-exim4.conf.conf.erb'),
     ensure  => file,
     require => Package['exim'],
+    notify  => Exec['update-exim4.conf'],
   }
+  file {'/etc/mailname':
+    ensure => file,
+    content => $mail_domain,
+    notify => Exec['update-exim4.conf'],
+  }
+
+  exec {'update-exim4.conf':
+    refreshonly => true,
+  }
+
   package {'mailx':
     name   => 'bsd-mailx',
     ensure => present,
+  }
+  package {'heirloom-mailx':
+    ensure => absent,
+    require => Package['mailx'],
   }
 }
