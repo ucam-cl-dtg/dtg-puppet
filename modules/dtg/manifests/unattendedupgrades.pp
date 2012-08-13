022e6348e9f04464e96584160ae939844ae04a08
Daniel Thomas <drt24@cam.ac.uk>
2012-08-13 15:18:00 +0100

    Specify owner and group for unattended upgrades files
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- modules/dtg/manifests/unattendedupgrades.pp
+++ modules/dtg/manifests/unattendedupgrades.pp
@@ -5,9 +5,13 @@ class dtg::unattendedupgrades ($unattended_upgrade_notify_emailaddress) {
   file {'/etc/apt/apt.conf.d/10periodic':
     ensure => file,
     source => 'puppet:///modules/dtg/apt/10periodic',
+    owner  => 'root',
+    group  => 'adm',
   }
   file {'/etc/apt/apt.conf.d/50unattended-upgrades':
     ensure  => file,
     content => template('dtg/apt/50unattended-upgrades.erb'),
+    owner   => 'root',
+    group   => 'adm',
   }
 }
