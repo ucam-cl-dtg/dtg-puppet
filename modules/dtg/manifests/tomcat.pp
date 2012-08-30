d0d1950f271237a4697c0c26fe8b9bf7afcd1fea
Daniel Thomas <drt24@cam.ac.uk>
2012-08-30 14:06:14 +0100

    Add ucam ppa to tomcat
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- modules/dtg/manifests/tomcat.pp
+++ modules/dtg/manifests/tomcat.pp
@@ -9,6 +9,7 @@ class dtg::tomcat ($version = '6'){
   }
 }
 class dtg::tomcat::raven inherits dtg::tomcat {
+  class {'dtg::tomcat::raven::repo': stage => 'repos'}
   file { "/etc/$tomcat/raven_pubkey.crt":
     source => 'puppet:///modules/dtg/raven/pubkey2.crt',
     mode   => 644,
@@ -25,9 +26,11 @@ class dtg::tomcat::raven inherits dtg::tomcat {
   }
   package{'libucamwebauth-java':
     ensure => installed,
+    require => Apt::Ppa['ppa:ucam-cl-dtg/ucam'],
   }
   package{ 'libucamwebauth-tomcat-java':
     ensure => installed,
+    require => Package['libucamwebauth-java'],
   }
   $tomcatlib = "/usr/share/${tomcat}/lib/"
   file {"${tomcatlib}/webauth.jar":
@@ -56,4 +59,7 @@ class dtg::tomcat::raven inherits dtg::tomcat {
     target => '/usr/share/java/commons-codec.jar',
     require => Package['libcommons-codec-java'],
   }
+}
+class dtg::tomcat::raven::repo {
+  apt::ppa {'ppa:ucam-cl-dtg/ucam': }
 }
