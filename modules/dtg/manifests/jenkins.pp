4af528c40396eef3fb68518fd8b1cfe8dad3a293
Daniel Thomas <drt24@cam.ac.uk>
2012-08-30 17:38:34 +0100

    Add directories required by jenkins to run some jobs
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- modules/dtg/manifests/jenkins.pp
+++ modules/dtg/manifests/jenkins.pp
@@ -32,9 +32,42 @@ class dtg::jenkins {
     require_password => false,
     comment          => 'Allow tomcat to build debian packages using cowbuilder in a chroot',
   }
+  file { '/usr/share/tomcat6/.config/':
+    ensure => directory,
+    owner  => 'tomcat6',
+    group  => 'tomcat6',
+    mode   => '0775',
+    require => Package['jenkins-tomcat'],
+  }
+  file { '/usr/share/tomcat6/.m2/':
+    ensure => directory,
+    owner  => 'tomcat6',
+    group  => 'tomcat6',
+    mode   => '0775',
+    require => Package['jenkins-tomcat'],
+  }
+  file { '/usr/share/tomcat6/.android/':
+    ensure => directory,
+    owner  => 'tomcat6',
+    group  => 'tomcat6',
+    mode   => '0775',
+  }
+  file { '/usr/share/tomcat6/.ssh/':
+    ensure => directory,
+    owner  => 'tomcat6',
+    group  => 'tomcat6',
+    mode   => '0700',
+  }
+  file { '/usr/share/tomcat6/.ssh/config':
+    ensure => file,
+    owner  => 'tomcat6',
+    group  => 'tomcat6',
+    mode   => '0644',
+  }
   #TODO(drt24) Default to java7
-  # Modify /etc/jenkins/debian_gule to point at precise and have main and universe components
+  # Modify /etc/jenkins/debian_glue to point at precise and have main and universe components
   # Restore jenkins jobs from backups
+  # /usr/share/tomcat6/{.ssh/{id_rsa,id_rsa.pub},.m2/settings.xml,.android/debug.keystore} need to be got from secrets server
 }
 # So that we can appy a stage to it
 class dtg::jenkins::repos {
