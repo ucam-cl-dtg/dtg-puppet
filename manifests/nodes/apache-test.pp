d2c5d47aa36f1894006a7358832f5285a4fe843d
Daniel Thomas <drt24@cam.ac.uk>
2012-09-07 15:57:55 +0100

    Apache test config for demo

---
+++ manifests/nodes/apache-test.pp
@@ -0,0 +1,16 @@
+node 'apache-test.dtg.cl.cam.ac.uk' {
+  include minimal
+  class {'apache': }
+  class { 'dtg::apache::raven': server_description => "James apache raven",}
+  
+}
+if ( $::fqdn == $::nagios_machine_fqdn ) {
+  nagios_monitor { 'apache-test':
+    parents    => '',
+    address    => 'apache-test.dtg.cl.cam.ac.uk',
+    hostgroups => [ 'ssh-servers', 'http-servers'],
+  }
+}
+if ( $::fqdn == $::munin_machine_fqdn ) {
+  munin::gatherer::configure_node { 'apache-test': }
+}
