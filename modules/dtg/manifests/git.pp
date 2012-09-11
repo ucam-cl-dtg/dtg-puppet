3473669ac9d85bfcb0d22a64f30e227d9ad37089
Daniel Thomas <drt24@cam.ac.uk>
2012-09-11 18:12:44 +0100

    Add mysql and sqlite3 dev headers required by bundle install
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- modules/dtg/manifests/git.pp
+++ modules/dtg/manifests/git.pp
@@ -24,7 +24,7 @@ class dtg::git {
   }
   #TODO(drt24) setup backups and restore from backups
   # Setup gitlab
-  $gitlabpackages = ['ruby','rubygems','ruby-bundler','python-pygments','libicu-dev']
+  $gitlabpackages = ['ruby','rubygems','ruby-bundler','python-pygments','libicu-dev','libmysqlclient-dev', 'ruby-sqlite3', 'libsqlite3-dev']
   package {$gitlabpackages :
     ensure => installed,
   }
@@ -143,7 +143,7 @@ class dtg::git {
     command => 'sudo -u gitlab -g gitlab -H bundle install --without development test --deployment',
     unless  => 'false',#TODO(drt24)
     cwd     => '/srv/gitlab/gitlab/',
-    require => [File['/srv/gitlab/gitlab/config/gitlab.yml'],Class['mysql::ruby']],
+    require => [File['/srv/gitlab/gitlab/config/gitlab.yml'],Class['mysql::ruby'],Package['libmysqlclient-dev']],
   }
   exec {'setup gitlab database':
     command => 'sudo -u gitlab -g gitlab -H bundle exec rake gitlab:app:setup RAILS_ENV=production',
