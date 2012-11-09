cc3745ac55c6613eea54fdb0a3feabdeae972f8b
Lucian Carata <lc525@cam.ac.uk>
2012-11-09 18:32:57 +0000

    add git_wiki to test-weather node

--- manifests/nodes/test-weather.pp
+++ manifests/nodes/test-weather.pp
@@ -1,5 +1,6 @@
 node 'test-weather.dtg.cl.cam.ac.uk' {
   include 'dtg::minimal'
+  include 'dtg::git_wiki' 
 }
 # We don't actually mind if test-puppet goes down and we might take it down
 # deliberately and not want emails triggered
