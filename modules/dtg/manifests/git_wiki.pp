a62dce7fdf595f17dbb422d4c8777630fcdd221e
Lucian Carata <lc525@cam.ac.uk>
2012-11-11 17:59:56 +0000

    fix: passenger configuration

--- modules/dtg/manifests/git_wiki.pp
+++ modules/dtg/manifests/git_wiki.pp
@@ -26,15 +26,15 @@ class dtg::git::gollum::pre {
       ensure => present,
       require => Rvm_system_ruby['ruby-1.9.3-p194'];
     }
-    #class{
-#	    'rvm::passenger::apache':
-#	    version => '3.0.18',
-#	    ruby_version => 'ruby-1.9.3-p194',
-#	    mininstances => '3',
-#	    maxinstancesperapp => '0',
-#	    maxpoolsize => '30',
-#	    spawnmethod => 'smart-lv2';
-#    }
+    class{
+	    'rvm::passenger::apache':
+	    version => '3.0.18',
+	    ruby_version => 'ruby-1.9.3-p194',
+	    mininstances => '3',
+	    maxinstancesperapp => '0',
+	    maxpoolsize => '30',
+	    spawnmethod => 'smart-lv2';
+    }
 	    
 }
 
