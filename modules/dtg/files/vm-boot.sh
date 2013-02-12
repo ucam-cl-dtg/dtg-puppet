dce264cf7ef54d8ae37822bc22e4de3583472ea5
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2013-02-12 19:17:00 +0000

    This patch looks to see if the VM being created has a hostname of puppy*, if so we then import Dom0's public key, so that scripts in Dom0 can change the hostname to something better.

--- modules/dtg/files/vm-boot.sh
+++ modules/dtg/files/vm-boot.sh
@@ -2,6 +2,8 @@
 
 PUPPETBARE=/etc/puppet-bare
 BOOTSTRAP="https://raw.github.com/ucam-cl-dtg/dtg-puppet/master/modules/dtg/files/bootstrap.sh"
+AUTHORIZED_KEYS="/home/dtg/.ssh/authorized_keys"
+DOM0_PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAujx2sop6KNPYr6v/IWEpFITi964d89N3uVowvRo3X5f9a7fiquEphaXIvrMoF74TtmWe78NybfPPgKgTdmaBWYxhbykO7aC9QeY+iDcQqKWrLFlBAbqJ6GYJYfiSM0DZbmAXiAuguNhX1LU51zPRVKYf2/yAgCmJv2yammXppwCE+BJvBVqJziy2Cs0PKhI/26Altelc2tH+SMIlF9ZuSKCtAcyMTPQxTVrJ/zilmceh/U3LcLD3OlOD7XfHxUQ+fiH0KZ27dja6mnsb/OAvmqpmD8mYZs2vTUiFRH9V6HmQqQRO82a6XRRK6wHcGnh+J7JW45dO75lmtBElw1djyw== root@husky0.dtg.cl.cam.ac.uk"
 
 # if there is a /dev/xvdb without partitions, let's use it
 
@@ -22,4 +24,20 @@ if [ ! -d $PUPPETBARE ]; then
     wget ${BOOTSTRAP}
     chmod +x bootstrap.sh
     ./bootstrap.sh
+fi
+
+
+# if the hostname is puppy* then we want to import dom0's key so
+# scripts can SSH in and sort this out. We don't want dom0 to
+# monkeysphere. We also generate a new fingerprint
+
+if [ $(echo $HOSTNAME | grep puppy) ]; then
+    ssh-keygen -l -f /etc/ssh/ssh_host_rsa_key.pub
+
+    mkdir -p /home/dtg/.ssh/
+    echo "${DOM0_PUBLIC_KEY}" >> $AUTHORIZED_KEYS
+
+    monkeysphere-authentication update-users
+else
+    sed -i "/${DOM0_PUBLIC_KEY}/d"
 fi
