d1667fd6f9389e783520668f3923e45ef4d45a3a
Daniel Thomas <drt24@cam.ac.uk>
2013-02-13 16:11:18 +0000

    Don't install gradle everywhere - just on jenkins. Careful with scopes oc243.

--- manifests/nodes/jenkins-master.pp
+++ manifests/nodes/jenkins-master.pp
@@ -2,19 +2,19 @@ node 'jenkins-master.dtg.cl.cam.ac.uk' {
   include 'dtg::minimal'
   class { 'dtg::jenkins': }
   class { 'sonar': version => '3.2'}
+  package { "gradle": ensure => "installed" }
+
+  wget::fetch { "download-gradle-plugin":
+    source => "\"https://updates.jenkins-ci.org/download/plugins/gradle/1.21/gradle.hpi\"",
+    destination => "/var/lib/jenkins/plugins/gradle.hpi"
+  }
+
+  wget::fetch { "gradlew setup":
+    source => "\"http://services.gradle.org/distributions/gradle-1.4-all.zip\"",
+    destination => "/var/lib/jenkins/workspace/external/gradle/gradle-1.4-all.zip"
+  }
 }
 
-package { "gradle": ensure => "installed" }
-
-wget::fetch { "download-gradle-plugin":
-  source => "\"https://updates.jenkins-ci.org/download/plugins/gradle/1.21/gradle.hpi\"",
-  destination => "/var/lib/jenkins/plugins/gradle.hpi"
-}
-
-wget::fetch { "gradlew setup":
-  source => "\"http://services.gradle.org/distributions/gradle-1.4-all.zip\"",
-  destination => "/var/lib/jenkins/workspace/external/gradle/gradle-1.4-all.zip"
-}
 
 if ( $::fqdn == $::nagios_machine_fqdn ) {
   nagios::monitor { 'jenkins-master':
