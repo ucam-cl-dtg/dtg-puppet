e47c3aa85643e3a80a62cb48f5baf5fa7d4a4588
Daniel Thomas <drt24@cam.ac.uk>
2013-02-14 10:27:37 +0000

    jenkins: move gradle config to jenkins.pp rather than the node file so that it is with all the other jenkins config
    Also fix it to have the correct dependencies

--- manifests/nodes/jenkins-master.pp
+++ manifests/nodes/jenkins-master.pp
@@ -2,17 +2,6 @@ node 'jenkins-master.dtg.cl.cam.ac.uk' {
   include 'dtg::minimal'
   class { 'dtg::jenkins': }
   class { 'sonar': version => '3.2'}
-  package { "gradle": ensure => "installed" }
-
-  wget::fetch { "download-gradle-plugin":
-    source => "\"https://updates.jenkins-ci.org/download/plugins/gradle/1.21/gradle.hpi\"",
-    destination => "/var/lib/jenkins/plugins/gradle.hpi"
-  }
-
-  wget::fetch { "gradlew setup":
-    source => "\"http://services.gradle.org/distributions/gradle-1.4-all.zip\"",
-    destination => "/var/lib/jenkins/workspace/external/gradle/gradle-1.4-all.zip"
-  }
 }
 
 
