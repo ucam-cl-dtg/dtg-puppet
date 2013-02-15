b488039af1a807564b2101d05fdd37c0a7c5c75b
Mattias Linnap <mattias@linnap.com>
2013-02-15 11:48:50 +0000

    Removed network config from yousense node manifest.

--- manifests/nodes/yousense.pp
+++ manifests/nodes/yousense.pp
@@ -1,29 +1,6 @@
 node 'yousense.dtg.cl.cam.ac.uk' {
   include 'dtg::minimal'
 
-  # Network Setup
-
-  class { 'network::interfaces':
-    interfaces => {
-      'eth0' => {
-        'method' => 'static',
-        'address' => '128.232.98.171',
-        'netmask' => '255.255.255.0',
-        'gateway' => '128.232.98.1',
-      }
-    },
-    auto => ["eth0"],
-  }
-
-  $dns = 'nameserver 128.232.1.1\nnameserver 128.232.1.2\nnameserver 128.232.1.3\nsearch dtg.cl.cam.ac.uk'
-  file { '/etc/resolv.conf':
-    content => '$dns',
-  }
-  file { '/etc/resolvconf/resolv.conf.d/original':
-    content => '$dns',
-  }
-
-
   # Package Setup
 
   package {
