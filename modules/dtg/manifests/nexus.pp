1c282fbd5027b7e30d164aff428ceb85f9e55f7a
Andrew Rice <acr31@cam.ac.uk>
2013-03-08 13:52:37 +0000

    updated to fix broken URL and to move parameters to default arguments

--- modules/dtg/manifests/nexus.pp
+++ modules/dtg/manifests/nexus.pp
@@ -1,9 +1,19 @@
-define dtg::nexus::fetch ($artifact_name, $artifact_version, $artifact_type, $artifact_classifier, $destination_directory) {
+define dtg::nexus::fetch (
+       $artifact_name, 
+       $artifact_version, 
+       $artifact_type = "ANY", 
+       $artifact_classifier, 
+       $destination_directory,
+       $nexus_server_name = "http://dtg-maven.cl.cam.ac.uk",
+       $nexus_user = "dtg",
+       $nexus_password = "PetliujyowzaddOn",
+       $groupID = "uk.ac.cam.cl.dtg"
+       ) {
   if $artifact_version =~ /-SNAPSHOT/ {
       $repository = "snapshot"
   }
   else {
-      $repository = "release"
+      $repository = "releases"
   }
   
   if $artifact_classifier =~ /ANY/ {
@@ -13,15 +23,11 @@ define dtg::nexus::fetch ($artifact_name, $artifact_version, $artifact_type, $ar
         $classifier = "&c=$artifact_classifier"
   }
 
-  $nexus_server_name = "http://dtg-maven.cl.cam.ac.uk/"
-  $nexus_user = "dtg"
-  $nexus_password = "PetliujyowzaddOn"
-  $groupID = "uk.ac.cam.cl.dtg"
   file {$destination_directory:
       ensure => directory
   } ->     
   wget::authfetch { "nexus-fetch":
-        source => "\"${nexus_server_name}/service/local/artifact/maven/redirect?r=${repository}&g=${groupid}&a=${artifact_name}&v=${artifact_version}&e=${artifact_type}${classifier}\"",
+        source => "\"${nexus_server_name}/service/local/artifact/maven/redirect?r=${repository}&g=${groupID}&a=${artifact_name}&v=${artifact_version}&e=${artifact_type}${classifier}\"",
         destination => "${destination_directory}/${artifact_name}-${artifact_version}.${artifact_type}",
         user => $nexus_user,
         password => $nexus_password
