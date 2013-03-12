a67c728125f0ce4485b9b0386ff128368f24f99a
Mattias Linnap <mattias@linnap.com>
2013-03-12 12:44:35 +0000

    Directory for data files on local disk

--- manifests/nodes/ml421-crunch.pp
+++ manifests/nodes/ml421-crunch.pp
@@ -22,7 +22,7 @@ node 'ml421-crunch' {
         gid => 'positium',
         groups => ['positium'],
     }
-    file { ['/home/positium', '/home/positium/.ssh']:
+    file { ['/home/positium', '/home/positium/.ssh', '/local/data/positium']:
         ensure => directory,
         owner => 'positium',
         group => 'positium',
@@ -33,6 +33,5 @@ node 'ml421-crunch' {
         mode => '0600',
         content => 'from="*.cam.ac.uk,*.ee" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCYc3EzVVeQZAiNHyk1v5f8ckOhwjk1I6B362I7Wy13MKNxBb+8KN2dKH7qFWPN5nj1e8tU7l3JoXcoWS9LvrhigfJXmyZh4No5L8Xrr0hfD0j6UXCq5nlsBFJW/t88RogNxdLwYVrJxA/5EdJJFxKlmUw8GtdmVuYljqluqn7eOLxL2ZxsC/MVTMQcMlNLXEEzBGvmtpLlEhjqO0aYsSXu1Ol2LxrJF+pyhidLnGMno76PytSGyWuOs6qpztSkBXprcr0RFYjV8l5XJjvW03enY6K9ShqDoGE0QnDLxKScjmzJ/FshxbNljGqKa54fb+K9uWLiAWwk4TqwHOjT3doV',
     }
-
 }
 
