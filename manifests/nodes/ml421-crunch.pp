7661151c475dd320c5ae1bf1e407bcda72587bd3
Mattias Linnap <mattias@linnap.com>
2013-03-12 12:42:41 +0000

    Changed positium public key for ml421-crunch

--- manifests/nodes/ml421-crunch.pp
+++ manifests/nodes/ml421-crunch.pp
@@ -31,7 +31,7 @@ node 'ml421-crunch' {
     file { '/home/positium/.ssh/authorized_keys':
         ensure => file,
         mode => '0600',
-        content => 'from="*.cam.ac.uk,*.ee" ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAvy3tVsuO7ibMBcJ5NY3xdc8f8yPHNYTu4VPpooDo0MiThj+BE/sSBGsklKPqw6bmPO9LBatItMUqNXWb9qdTwdAEZXGHcnxb/s2V07I6S+xN8iq10AlLTlaawPadXqK7eG1clrxCSnccZ7tJHtnID0nb2Tsh7OZATANeikJS8TEHv4/v9SWqpg2CpcG3Jd2UqQ2BBbdNTJ3t72wXl4BvaSKl+A1vPSjfZ5DqY5a5U/xZz2f3cV5CYdebzXdbVHnw8NaQzXqa8CsyVq8eLj28rb7ytGdj5HRslTuOdsXf8u2LiP+Q/oupW9giZ+DutLouSJjUMy3Wx1Mzzh83XygqeQ==',
+        content => 'from="*.cam.ac.uk,*.ee" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCYc3EzVVeQZAiNHyk1v5f8ckOhwjk1I6B362I7Wy13MKNxBb+8KN2dKH7qFWPN5nj1e8tU7l3JoXcoWS9LvrhigfJXmyZh4No5L8Xrr0hfD0j6UXCq5nlsBFJW/t88RogNxdLwYVrJxA/5EdJJFxKlmUw8GtdmVuYljqluqn7eOLxL2ZxsC/MVTMQcMlNLXEEzBGvmtpLlEhjqO0aYsSXu1Ol2LxrJF+pyhidLnGMno76PytSGyWuOs6qpztSkBXprcr0RFYjV8l5XJjvW03enY6K9ShqDoGE0QnDLxKScjmzJ/FshxbNljGqKa54fb+K9uWLiAWwk4TqwHOjT3doV',
     }
 
 }
