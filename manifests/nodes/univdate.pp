d276f925edb4eab16e919d7f3cd2f76c36286e00
Andrew Rice <acr31@cam.ac.uk>
2013-03-30 16:09:21 +0000

    Initial checkin of a puppet config for the univdate service which I need to write...(this service provides a REST api on the university calendar)

---
+++ manifests/nodes/univdate.pp
@@ -0,0 +1,27 @@
+node /univdate(-\d+)?/ {
+  include 'dtg::minimal'
+
+  class {'apache': }
+  ->
+  apache::module {'proxy':}
+  ->
+  apache::module {'proxy_http':}
+  ->
+  apache::site {'univdate':
+    source => 'puppet:///modules/dtg/apache/univdate.conf',
+  }
+  
+  $servlet_version = "1.0.0-SNAPSHOT"
+
+  class {'dtg::tomcat': version => '7'}
+  ->
+  dtg::nexus::fetch{"download-servlet":
+    artifact_name => "univdate",
+    artifact_version => $servlet_version,
+    artifact_type => "war",
+    destination_directory => "/usr/local/share/univdate-servlet",
+    symlink => "/var/lib/tomcat7/webapps/univdate.war",
+  }
+  
+  class {'dtg::firewall::publichttp':}
+}
