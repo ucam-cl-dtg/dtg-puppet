a42209e7be7dae718ab51d1a4e9edd789cffe93e
Andrew Rice <acr31@cam.ac.uk>
2013-05-12 16:34:16 +0100

    Added configuration for the nas01 nfs server and firewall rules

--- manifests/nodes/nas01.pp
+++ manifests/nodes/nas01.pp
@@ -15,6 +15,98 @@ node /nas01/ {
   file{"/etc/fancontrol":
     source => 'puppet:///modules/dtg/fancontrol/nas01'
   }
+
+  # We have to tell the NFS server to use a particular set of ports
+  # and then open the relevant firewall holes
+  include 'nfs::server'
+  file{"/etc/default/nfs-kernel-server":
+    source => 'puppet:///modules/dtg/nfs/nas-nfs-kernel-server',
+    notify => Service['nfs-kernel-server']
+  }
+  ->
+  firewall { '030-nfs accept tcp 111 (sunrpc) from dtg':
+    proto   => 'tcp',
+    dport   => '111',
+    source  => $::local_subnet,
+    action  => 'accept',
+  }
+  ->
+  firewall { '031-nfs accept udp 111 (sunrpc) from dtg':
+    proto   => 'udp',
+    dport   => '111',
+    source  => $::local_subnet,
+    action  => 'accept',
+  }
+  ->
+  firewall { '032-nfs accept tcp 2049 (nfs) from dtg':
+    proto   => 'tcp',
+    dport   => '2049',
+    source  => $::local_subnet,
+    action  => 'accept',
+  }
+  ->
+  firewall { '033-nfs accept tcp 32803 (lockd) from dtg':
+    proto   => 'tcp',
+    dport   => '32803',
+    source  => $::local_subnet,
+    action  => 'accept',
+  }
+  ->
+  firewall { '034-nfs accept udp 32769 (lockd) from dtg':
+    proto   => 'udp',
+    dport   => '32769',
+    source  => $::local_subnet,
+    action  => 'accept',
+  }
+  ->
+  firewall { '035-nfs accept tcp 892 (mountd) from dtg':
+    proto   => 'tcp',
+    dport   => '892',
+    source  => $::local_subnet,
+    action  => 'accept',
+  }
+  ->
+  firewall { '036-nfs accept udp 892 (mountd) from dtg':
+    proto   => 'udp',
+    dport   => '892',
+    source  => $::local_subnet,
+    action  => 'accept',
+  }
+  ->
+  firewall { '037-nfs accept tcp 875 (rquotad) from dtg':
+    proto   => 'tcp',
+    dport   => '875',
+    source  => $::local_subnet,
+    action  => 'accept',
+  }
+  ->
+  firewall { '038-nfs accept udp 875 (rquotad) from dtg':
+    proto   => 'udp',
+    dport   => '875',
+    source  => $::local_subnet,
+    action  => 'accept',
+  }
+  ->
+  firewall { '039-nfs accept tcp 662 (statd) from dtg':
+    proto   => 'tcp',
+    dport   => '662',
+    source  => $::local_subnet,
+    action  => 'accept',
+  }
+  ->
+  firewall { '039-nfs accept udp 662 (statd) from dtg':
+    proto   => 'udp',
+    dport   => '662',
+    source  => $::local_subnet,
+    action  => 'accept',
+  }
+  ->
+  nfs::export{"/data":
+    export => {
+      # host           options
+      "${::local_subnet}" => 'rw,sync,root_squash,no_subtree_check'
+    },
+  }
 }
 
 if ( $::fqdn == $::nagios_machine_fqdn ) {
