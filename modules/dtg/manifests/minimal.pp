27ab69e45291d6804a482b344c12bfee410e4abd
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2013-06-26 15:24:29 +0100

    Install NFS on the world

--- modules/dtg/manifests/minimal.pp
+++ modules/dtg/manifests/minimal.pp
@@ -14,7 +14,7 @@ class dtg::minimal ($manageapt = true) {
   }
 
   # Packages which should be installed on all servers
-  $packagelist = ['traceroute', 'vim', 'screen', 'fail2ban', 'curl', 'tar', 'runit', 'apg', 'emacs23-nox', 'htop']
+  $packagelist = ['traceroute', 'vim', 'screen', 'fail2ban', 'curl', 'tar', 'runit', 'apg', 'emacs23-nox', 'htop', 'nfs-common']
   package {
     $packagelist:
       ensure => installed
