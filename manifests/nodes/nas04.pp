33de49467f68d6ec0e717953cd8847ea6430305c
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2013-07-15 14:46:36 +0100

    Enable monitoring of nas04.
    We need to get this working before we go live, as we need to get munin
    zfs plugins working

--- manifests/nodes/nas04.pp
+++ manifests/nodes/nas04.pp
@@ -9,3 +9,15 @@ node "nas04.dtg.cl.cam.ac.uk" {
   }
 
 }
+
+if ( $::fqdn == $::nagios_machine_fqdn ) {
+  nagios::monitor { 'nas04':
+    parents    => '',
+    address    => 'nas04.dtg.cl.cam.ac.uk',
+    hostgroups => [ 'ssh-servers' ],
+  }
+}
+
+if ( $::fqdn == $::munin_machine_fqdn ) {
+  munin::gatherer::configure_node { 'nas04': }
+}
