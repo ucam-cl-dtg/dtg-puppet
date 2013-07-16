e00dde3fc62d0a1e6cb1c54313d2a4d20b53a7ec
Andrew Rice <acr31@cam.ac.uk>
2013-07-16 14:17:08 +0100

    Minor syntactic fix

--- manifests/nodes/nas04.pp
+++ manifests/nodes/nas04.pp
@@ -2,7 +2,7 @@ node "nas04.dtg.cl.cam.ac.uk" {
   include 'dtg::minimal'
   include 'nfs::server'
   
-  class {'dtg::zfs'}
+  class {'dtg::zfs': }
   
   class {'zfs_auto_snapshot':
     pool_names => [ 'dtg-pool' ]
