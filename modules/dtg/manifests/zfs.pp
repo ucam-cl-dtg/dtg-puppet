02bc0e980688a6c5b286329dacfcab112f3789b1
Oliver Chick <oc243@cam.ac.uk>
2013-07-16 14:51:20 +0100

    Add munin plugins to /usr/share/munin/plugins.
    These are the various plugins from munin-contrib, but ported so as to work with Linux.
    
    Still todo:
    0) Setup /etc/sudoers.d/zfs with puppet
    1) symlink from /etc/munin/plugins

--- modules/dtg/manifests/zfs.pp
+++ modules/dtg/manifests/zfs.pp
@@ -4,6 +4,29 @@ class dtg::zfs {
     ensure => present,
     require => Apt::Ppa['ppa:zfs-native/stable'],
   }
+  file {'/usr/share/munin/plugins/zlist':
+    ensure => file,
+    owner  =>  'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/dtg/zfs/zlist',
+  }
+
+  file {'/usr/share/munin/plugins/zfs-filesystem-graph':
+    ensure => file,
+    owner  =>  'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/dtg/zfs/zfs-filesystem-graph',
+  }
+
+  file {'/usr/share/munin/plugins/zpool_iostat':
+    ensure => file,
+    owner  =>  'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/dtg/zfs/zpool_iostat',
+  }  
 }
 
 class dtg::zfs::repos {
