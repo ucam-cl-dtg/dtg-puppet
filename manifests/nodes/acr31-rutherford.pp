6474bcdbe7550d3912b398dc9027272add92e821
Andrew Rice <acr31@cam.ac.uk>
2013-08-05 10:10:51 +0100

    Initial puppet config for rutherford-dev (physics teaching project) server

---
+++ manifests/nodes/acr31-rutherford.pp
@@ -0,0 +1,30 @@
+node /acr31-rutherford(-\d+)?/ {
+  include 'dtg::minimal'
+  
+  class {'dtg::tomcat': version => '7'}
+  
+  class {'dtg::firewall::publichttp':}
+
+  class { 'postgresql::server': 
+    config_hash => { 
+      'ip_mask_deny_postgres_user' => '0.0.0.0/0', 
+      'ip_mask_allow_all_users' => '127.0.0.1/32', 
+      'listen_addresses' => '*', 
+      'ipv4acls' => ['hostssl all all 127.0.0.1/32 md5']
+    }
+  }
+  ->
+  postgresql::db{'rutherford':
+    user => "rutherford",
+    password => "rutherford",
+    charset => "UTF-8",
+    grant => "ALL"
+  }
+
+  firewall { '011 accept all http on 8080':
+    proto   => 'tcp',
+    dport   => '8080',
+    action  => 'accept',
+  }
+  
+}
