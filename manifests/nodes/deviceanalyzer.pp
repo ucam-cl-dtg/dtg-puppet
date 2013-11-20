d20a7c4f45b35317aac1d250ba8d6cdf60ba7c85
Daniel Wagner <dtw30@cam.ac.uk>
2013-11-20 17:33:06 +0000

    don't sabotage mounting nas02 on push

--- manifests/nodes/deviceanalyzer.pp
+++ manifests/nodes/deviceanalyzer.pp
@@ -19,13 +19,7 @@ node 'deviceanalyzer.dtg.cl.cam.ac.uk' {
       ensure => installed
   }
 
-  # mount nas02
-  file { '/nas2':
-  	ensure => directory,
-  	owner => 'root',
-    group => 'root',
-    mode  => '0000', # we don't want *anyone* to actually write to that directory, ever. We just want it as a mount point.
-  }->
+  # mount nas02 on startup
   file_line { 'mount nas02':
   	line => 'nas02.cl.cam.ac.uk:/volume1/deviceanalyzer /nas2 nfs defaults 0 0',
   	path => '/etc/fstab', 
