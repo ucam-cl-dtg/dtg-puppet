76bff7e2125fadeb73b266ec45959ae81762949e
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2013-11-22 11:25:03 +0000

    Monitor nas02.dtg rather than nas02.cl

--- manifests/nodes/nas02.pp
+++ manifests/nodes/nas02.pp
@@ -1,7 +1,7 @@
 if ( $::monitor ) {
   nagios::monitor { 'nas02':
     parents    => '',
-    address    => 'nas02.cl.cam.ac.uk',
+    address    => 'nas02.dtg.cl.cam.ac.uk',
     hostgroups => [ ],
   }
 }
