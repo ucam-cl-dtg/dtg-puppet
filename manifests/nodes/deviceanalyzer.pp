052b6ed4929ee97347b2c39bfd5ffbcc54be2700
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2013-12-12 16:58:37 +0000

    No longer monitor the retired hound3. Instead monitor the deviceanalyzer vm.

--- manifests/nodes/deviceanalyzer.pp
+++ manifests/nodes/deviceanalyzer.pp
@@ -73,26 +73,20 @@ if ( $::monitor ) {
     hostgroups => [ 'ssh-servers' ],
   }
   nagios::monitor { 'deviceanalyzer':
-    parents    => 'hound3',
+    parents    => 'deviceanalyzer',
     address    => 'deviceanalyzer.cl.cam.ac.uk',
-    hostgroups => [ 'http-servers' ],
+    hostgroups => [ 'http-servers', 'ssh-servers' ],
   }
   nagios::monitor { 'secure.deviceanalyzer':
-    parents    => 'hound3',
+    parents    => 'deviceanalyzer',
     address    => 'secure.deviceanalyzer.cl.cam.ac.uk',
     hostgroups => [ 'http-servers' ],
   }
   nagios::monitor { 'upload.deviceanalyzer':
-    parents    => 'hound3',
+    parents    => 'deviceanalyzer',
     address    => 'upload.deviceanalyzer.cl.cam.ac.uk',
     hostgroups => [ 'http-servers' ],
   }
-  nagios::monitor { 'deviceanalyzer.dtg':
-    parents    => '',
-    address    => 'deviceanalyzer.dtg.cl.cam.ac.uk',
-    hostgroups => [ 'ssh-servers', 'http-servers'],
-  }
-  munin::gatherer::configure_node { 'hound3': }
   munin::gatherer::configure_node { 'hound4': }
   munin::gatherer::configure_node { 'deviceanalyzer': }
 }
