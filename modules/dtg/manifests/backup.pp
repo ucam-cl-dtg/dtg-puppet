388a0b8bd565503be9a04104ba866b7a55d56a63
Daniel Thomas <drt24@cam.ac.uk>
2014-02-20 11:37:31 +0000

    Fix references to dtg-code.cl.cam.ac.uk to code.dtg.cl.cam.ac.uk

--- modules/dtg/manifests/backup.pp
+++ modules/dtg/manifests/backup.pp
@@ -18,7 +18,7 @@ define dtg::backup::serversetup ($backup_directory, $script_destination, $user, 
   }
   file_line {"${name} backup authorized_keys":
     ensure  => present,
-    line    => "from=\"${backup_hosts}\",command=\"${script_destination}\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzb/C7T6G+PQbVuc/uqp4YbBrMQ9JLoIrSPesNtSRlDg4ckrUIiWeAVt/NUYtIdE+7vl2YNbSU0IK8+ONsApnTNP+XjfEwjaIfZPwQRXtEGWCSs4QwN/ZihrhWNODYUP473HbOFvsVwBQSM7LxLBW6TB1ecjpPYC1ds1InOhJcfP752GqX5KkQ++mbUpb8YR6FQZTGiBKDD/LfD25PUblZkdp1kuiikD4GhMQrDmD1m+CycFpkK5l4idqNBohBA4RV9ton17kJHrD4gpZtYFwifRmdaXt6ageofw2GKM5MAMo5QNOezLDWDovOp4atHa1ZaUlCLF5or3QzNw+wHI9N git@dtg-code.cl.cam.ac.uk",
+    line    => "from=\"${backup_hosts}\",command=\"${script_destination}\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzb/C7T6G+PQbVuc/uqp4YbBrMQ9JLoIrSPesNtSRlDg4ckrUIiWeAVt/NUYtIdE+7vl2YNbSU0IK8+ONsApnTNP+XjfEwjaIfZPwQRXtEGWCSs4QwN/ZihrhWNODYUP473HbOFvsVwBQSM7LxLBW6TB1ecjpPYC1ds1InOhJcfP752GqX5KkQ++mbUpb8YR6FQZTGiBKDD/LfD25PUblZkdp1kuiikD4GhMQrDmD1m+CycFpkK5l4idqNBohBA4RV9ton17kJHrD4gpZtYFwifRmdaXt6ageofw2GKM5MAMo5QNOezLDWDovOp4atHa1ZaUlCLF5or3QzNw+wHI9N git@code.dtg.cl.cam.ac.uk",
     path    => "${home}.ssh/authorized_keys",
     require => File["${home}.ssh/authorized_keys", $script_destination],
   }
