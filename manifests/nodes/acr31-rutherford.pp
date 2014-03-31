51bbfb3ee106afe684b7e9460079925e0e1612e4
Stephen Cummins <sacummins@gmail.com>
2014-03-31 13:16:21 +0100

    Changed tomcat shell to use bash instead of rssh as we probably need to do more as this user

--- manifests/nodes/acr31-rutherford.pp
+++ manifests/nodes/acr31-rutherford.pp
@@ -4,7 +4,7 @@ node /acr31-rutherford(-\d+)?/ {
   class {'dtg::tomcat': version => '7'}
   ->
   user { 'tomcat7':
-    shell => '/usr/bin/rssh'
+    shell => '/usr/bin/bash'
   }
   
   class {'dtg::firewall::publichttp':}
