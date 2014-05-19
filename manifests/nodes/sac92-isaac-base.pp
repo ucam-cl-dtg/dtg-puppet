f5e33b10d50872975d939f795c01e5937743847e
Stephen Cummins <sacummins@gmail.com>
2014-05-19 16:38:20 +0100

    Added apache install directive to isaac servers & commented out 80->8080 redirect rule.

--- manifests/nodes/sac92-isaac-base.pp
+++ manifests/nodes/sac92-isaac-base.pp
@@ -3,6 +3,20 @@ node /(\w+-)?isaac(-\w+)?(.+)?/ {
   
   User<|title == sac92 |> { groups +>[ 'adm' ]}
   
+  class {'apache::ubuntu': } ->
+  apache::module {'cgi':} ->
+  apache::module {'proxy':} ->
+  apache::module {'proxy_http':} ->
+  apache::site {'isaac-server':
+    source => 'puppet:///modules/dtg/apache/isaac-server.conf',
+  } ->
+  file { "/var/isaac-app/app":
+    ensure => "directory",
+    owner  => "root",
+    group  => "root",
+    mode   => 644,
+  }
+  
   class {'dtg::tomcat': version => '7'}
   ->
   user { 'tomcat7':
@@ -17,13 +31,14 @@ node /(\w+-)?isaac(-\w+)?(.+)?/ {
   }
   ->
   file {'/usr/share/tomcat7/.ssh/authorized_keys':
+    # Note: This will give access to the jenkins server to enable deployments from the CI process.
     ensure => file,
     mode => '0644',        
     content => 'from="*.cl.cam.ac.uk" ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAlQzIFjqes3XB09BAS9+lhZ9QuLRsFzLb3TwQJET/Q6tqotY41FgcquONrrEynTsJR8Rqko47OUH/49vzCuLMvOHBg336UQD954oIUBmyuPBlIaDH3QAGky8dVYnjf+qK6lOedvaUAmeTVgfBbPvHfSRYwlh1yYe+9DckJHsfky2OiDkych9E+XgQ4GipLf8Cw6127eiC3bQOXPYdZh7uKnW6vpnVPFPF5K1dSaUo3GxcpYt3OsT3IqB640m8mgekWtOmCuAP+9IEBFmCozwpqLz+EWv6wtova7tbVCkrU2iJwTbJzOUCvWv5JHYjAi/pWNIsKnWpFF9+m4th26GY4Q== jenkins@dtg-ci.cl.cam.ac.uk',
    }
   
   class {'dtg::firewall::publichttp':}
-  class {'dtg::firewall::80to8080': private => false}
+  #class {'dtg::firewall::80to8080': private => false}
 
   class { 'postgresql::globals':
     version => '9.3'
