3bd0aab82e556eeb2cded8a55fdaf823ee1e16c1
Stephen Cummins <sacummins@gmail.com>
2014-05-30 10:34:10 +0100

    Added tomcat7 memory change

--- manifests/nodes/sac92-isaac-base.pp
+++ manifests/nodes/sac92-isaac-base.pp
@@ -11,6 +11,13 @@ node /(\w+-)?isaac(-\w+)?(.+)?/ {
     mode   => 644,
   }
   ->
+  file { "/local/data/rutherford/keys/":
+    ensure => "directory",
+    owner  => "tomcat7",
+    group  => "tomcat7",
+    mode   => 640,
+  }
+  ->
   file { "/local/data/rutherford/git-contentstore":
     ensure => "directory",
     owner  => "tomcat7",
@@ -56,6 +63,13 @@ node /(\w+-)?isaac(-\w+)?(.+)?/ {
     mode => '0644',        
     content => 'from="*.cl.cam.ac.uk" ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAlQzIFjqes3XB09BAS9+lhZ9QuLRsFzLb3TwQJET/Q6tqotY41FgcquONrrEynTsJR8Rqko47OUH/49vzCuLMvOHBg336UQD954oIUBmyuPBlIaDH3QAGky8dVYnjf+qK6lOedvaUAmeTVgfBbPvHfSRYwlh1yYe+9DckJHsfky2OiDkych9E+XgQ4GipLf8Cw6127eiC3bQOXPYdZh7uKnW6vpnVPFPF5K1dSaUo3GxcpYt3OsT3IqB640m8mgekWtOmCuAP+9IEBFmCozwpqLz+EWv6wtova7tbVCkrU2iJwTbJzOUCvWv5JHYjAi/pWNIsKnWpFF9+m4th26GY4Q== jenkins@dtg-ci.cl.cam.ac.uk',
    }
+   ->
+  file_line{"Tomcat Memory":
+    line => 'JAVA_OPTS="-Djava.awt.headless=true -Xms512m -Xmx1024m -XX:+UseConcMarkSweepGC"',
+    path=> "/etc/default/tomcat7",
+    notify =>Service['tomcat'],
+    matches => "JAVA_OPTS=.*",
+  }   
   
   class {'dtg::firewall::publichttp':}
 
