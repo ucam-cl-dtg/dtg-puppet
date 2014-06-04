5029671d6ded86862b2ac7905fc66390c841a9c7
Daniel Thomas <drt24@cam.ac.uk>
2014-06-04 12:58:19 +0100

    Mount nas01 on weather in order to ship backups to it

--- manifests/nodes/weather.pp
+++ manifests/nodes/weather.pp
@@ -2,6 +2,26 @@ node 'weather.dtg.cl.cam.ac.uk' {
   # We don't have a local mirror of debian to point at
   class { 'dtg::minimal': manageapt => false, }
   class {'dtg::firewall::publichttp':}
+
+  # Mount nas01 in order to ship backups there.
+  file {'/mnt/nas01':
+    ensure => directory,
+    owner  => 'weather',
+  } ->
+  package {'autofs':
+    ensure => present,
+  } ->
+  file {'/etc/auto.nas01':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => 'a=r',
+    contents => 'nas01	nas01.dtg.cl.cam.ac.uk:/data/weather',
+  } ->
+  file_line {'mount nas01':
+    line => '/mnt/nas01	/etc/auto.nas01',
+    path => '/etc/auto.master',
+  }
 }
 
 if ( $::monitor ) {
