514fa9bd5603e95b5fd908ce7c51126747b7a051
Stephen Cummins <sacummins@gmail.com>
2014-06-11 12:19:31 +0100

    Added some configuration for crucible.

--- manifests/nodes/crucible.pp
+++ manifests/nodes/crucible.pp
@@ -6,4 +6,28 @@ node 'crucible.dtg.cl.cam.ac.uk' {
   package {['openjdk-7-jdk']:
     ensure => installed
   }
+ ->
+  file { '/usr/lib/jvm/default-java':
+    ensure => 'link',
+    target => '/usr/lib/jvm/java-1.7.0-openjdk-amd64',
+  }
+ ->
+  file_line { 'java_home':
+    path => '/etc/environment',
+    line => 'JAVA_HOME="/usr/lib/jvm/default-java"',
+  }
+ ->
+ user { "crucible":
+    ensure => present,
+    comment => "Crucible user",
+    home => "/local/crucible_home"
+  }
+ ->
+ file { "/local/crucible_home":
+    ensure => "directory",
+    owner  => "crucible",
+    group  => "crucible",
+    mode   => 755,
+ }
+
 }
