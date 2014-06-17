f3b34ef1ff7a5d1ddf4444c6fb4e92e6f1031cab
DTG Infrastructure <dtg-infra@cl.cam.ac.uk>
2014-06-17 16:11:16 +0100

    More hacks to try and get lxc working

--- manifests/nodes/acr31-containers.pp
+++ manifests/nodes/acr31-containers.pp
@@ -1,6 +1,12 @@
 node /acr31-containers(-\d+)?/ {
   include 'dtg::minimal'
 
+  service { "gunicorn":
+    ensure => "running",
+    enable => "true",
+    require => Package["gunicorn"]
+  }
+  
   # I need python3 for the lxc api, but gunicorn is not packaged for it
   # Therefore we install the python2 version of gunicorn so as to get the nice
   # debian packaging and then symlink the python3 version (from source) over the top
@@ -9,30 +15,39 @@ node /acr31-containers(-\d+)?/ {
   package{$packages:
     ensure => installed,
   }
-
-  file{"/etc/gunicorn.d/containers":
-    source => 'puppet:///modules/dtg/gunicorn/containers',
+  ->
+  file{["/var/www","/var/www/.config","/var/www/.config/lxc"]:
+    ensure => directory,
+    mode => "u+rwx",
+    owner => "www-data",
   }
-    
-  class {'dtg::firewall::publichttp':}
-
-  service { "gunicorn":
-    ensure => "running",
-    enable => "true",
-    require => Package["gunicorn"]
+  ->
+  file{"/var/www/.config/lxc/default.conf":
+    source => 'puppet:///modules/dtg/lxc/default.conf',
   }
-  
+  ->
   exec{"install-gunicorn-python3":
     command => "pip3 install gunicorn",
     path => "/usr/bin/:/bin",
     unless => "pip3 freeze | grep gunicorn",
-  } ->
+  }
+  ->
   file{"/usr/bin/gunicorn":
     ensure => link,
     source => "/usr/local/bin/gunicorn",
     notify => Service["gunicorn"],
   }
-    
+  ->
+  file{"/etc/gunicorn.d/containers":
+    source => 'puppet:///modules/dtg/gunicorn/containers',
+  }
+  ->
+  exec{"namespace-permission":
+    command => "usermod -v 100000-200000 -w 100000-200000 www-data"
+  }
+  
+  class {'dtg::firewall::publichttp':}
+
   file{"/usr/local/containers-webapp-bare":
     ensure => directory,
     owner => acr31,
