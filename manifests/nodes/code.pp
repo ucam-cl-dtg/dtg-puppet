dcc098ff7aa50cb3bbf4a985c7b473b192e53d97
Daniel Thomas <drt24@cam.ac.uk>
2014-07-10 16:11:51 +0100

    Add Tom Lefley to maven sshmirror and urop2014

--- manifests/nodes/code.pp
+++ manifests/nodes/code.pp
@@ -16,7 +16,8 @@ node 'code.dtg.cl.cam.ac.uk' {
     'Ian Davies (ssh) <ipd21@cam.ac.uk>',
     'Ripduman Sohan (Cambridge Key) <ripduman.sohan@cl.cam.ac.uk>',
     'Stephen Cummins (Main key) <sacummins@gmail.com>',
-    'Kovacsics Robert (Alias "kr2") <kovirobi@gmail.com>'
+    'Kovacsics Robert (Alias "kr2") <kovirobi@gmail.com>',
+    'Tom Lefley <tl364@cam.ac.uk>'
     ]
   }
   class {'dtg::firewall::publichttp':}
--- manifests/nodes/urop2014.pp
+++ manifests/nodes/urop2014.pp
@@ -10,6 +10,12 @@ node 'urop2014.dtg.cl.cam.ac.uk' {
     keys      => 'Kovacsics Robert (Alias "kr2") <kovirobi@gmail.com>',
     uid       => 232265, # From MCS linux `getent passwd rmk35`
   }
+  dtg::add_user { 'tl364':
+    real_name => 'Tom Lefley',
+    groups    => [ 'adm' ],
+    keys      => 'Tom Lefley <tl364@cam.ac.uk>',
+    uid       => 204241, # From MCS linux `getent passwd tl364`
+  }
 
   # Specific config
   class {'dtg::tomcat': version => '7'}
