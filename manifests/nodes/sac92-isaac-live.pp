f33cbab0900eab9096e60b1ac618cdfc339f9746
Daniel Thomas <drt24@cam.ac.uk>
2014-09-02 11:45:56 +0100

    Fix isaac monitoring code to be in the right scope

--- manifests/nodes/sac92-isaac-live.pp
+++ manifests/nodes/sac92-isaac-live.pp
@@ -26,19 +26,19 @@ node /(\w+-)?isaac-live/ {
     user               => 'isaac',
     home               => '/usr/share/isaac/',
   }
-  
-  if ( $::monitor ) {
-    nagios::monitor { 'isaac-live':
-      parents    => 'nas04',
-      address    => 'isaac-live.dtg.cl.cam.ac.uk',
-      hostgroups => [ 'ssh-servers' , 'http-servers' ],
-    }
-    munin::gatherer::configure_node { 'isaac-live': }
+}
 
-    nagios::monitor { 'isaac-physics':
-      parents    => ['isaac-live', 'balancer'],
-      address    => 'isaacphysics.org',
-      hostgroups => ['https-servers'],
-    }
+if ( $::monitor ) {
+  nagios::monitor { 'isaac-live':
+    parents    => 'nas04',
+    address    => 'isaac-live.dtg.cl.cam.ac.uk',
+    hostgroups => [ 'ssh-servers' , 'http-servers' ],
+  }
+  munin::gatherer::configure_node { 'isaac-live': }
+
+  nagios::monitor { 'isaac-physics':
+    parents    => ['isaac-live', 'balancer'],
+    address    => 'isaacphysics.org',
+    hostgroups => ['https-servers'],
   }
 }
