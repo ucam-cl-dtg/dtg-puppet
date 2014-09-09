bf52f63cd92a73a46bc40f1a262a7420836bc5f0
Daniel Thomas <drt24@cam.ac.uk>
2014-09-09 17:32:25 +0100

    Support ssh tunnel forwarding of entropy.dtg's entropy to allow remote hosts to access it

--- manifests/nodes/entropy.pp
+++ manifests/nodes/entropy.pp
@@ -9,6 +9,12 @@ node 'entropy.dtg.cl.cam.ac.uk' {
   }
   # Allow connections to 7776
   class { 'dtg::firewall::entropy':}
+  dtg::sshtunnelhost{ 'entropy-ssh-tunnel':
+    username    => 'entropyssh',
+    home        => '/home/entropyssh',
+    destination => 'localhost:777',
+    keys        => ['grapevine-drt24 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHt9wYQcmbir0YXnlcCFco+NRBujO2lAd47jQevej2WhpxFKXfaqL0ffBlj/1UGkVdeGXa3tAiCDIFFHdcYJtj48Cy2YVm1KOyYtlS1LMdenqpYjZgyE6JT0UCGM+6sfQCcrqkgebkyDL1mpOrnY09zhHA0PQYeqLZgC5BOIQQsMSkX1ZuYh0XJEPlScGBET9nt5yvOubDazwb6/If06vyCmjF4BN/YQgkSwwUQgG2oTBb/EUVFUzkxSYXo2XNMVLMTO94TuvksB2/SdDPffYUTPPQ/iBNASy2agZbbV1PGM24Bu6zzY8ut5cpUUBoSlje4jkt/dnQvx3AwQ4cc7rbQgkHEUVeH+hj169qW4Xp4IZkcfGDZnM0lgu9/hBk913GuVObR/qI+S+Mp2nURM4OGDRIA+HvWXx/J4wxMUkQvCMJyijzlBkhKqFSOqrBleZJtVWyw23zRcalNtu7F9yloodox4crJBxd2ysrnIlntc1upiib3pG6QbOPWauk82/wSs1iRq1de28O0lzebB4QhFd5OAYkQhazpFTI2mXatdkgz0at/KuzPEZjeDAf9rpYm70KkI3MMe5n4USgFftXeo5fspo2AtjNSsonTm9YtI3TFDi4+AQLugEHUuvfbeH0CsCWSHemzbY1RaDyoQY+9u7Wl8lOq1y+GKG/EYSC0w=='],#TODO
+  }
 }
 if ( $::monitor ) {
   nagios::monitor { 'entropy':
---
+++ modules/dtg/manifests/sshtunnelhost.pp
@@ -0,0 +1,50 @@
+# keys array of arrays of "name type public key"
+define dtg::sshtunnelhost ($username, $home, $destination, $keys) {
+  group { $username: ensure => present, }
+  user { $username:
+    gid            => $username,
+    comment        => "SSH tunnel host user",
+    ensure         => present,
+#    purge_ssh_keys => true, # Only available in later versions of puppet
+    home           => $home,
+  }
+  file { $home:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => 'a+rx',
+  }
+  file { "${home}/.ssh":
+    ensure => directory,
+    owner  => $username,
+    group  => $username,
+    mode   => 'u+rx',
+  }
+  /* This will become useful in later versions of puppet but is not available in the 2.7 puppet installed on the entropy server
+
+  each($keys) |$keyarray| {
+    ssh_authorized_key { $keyarray[0]:
+      type    => $keyarray[1],
+      key     => $keyarray[2],
+      user    => $username,
+      ensure  => present,
+      options => ['no-pty','no-X11-forwarding',"permitopen=\"$destination\"",'command="/bin/echo do-not-send-commands"'],
+  }
+ */
+  # Hack to implement the above but without nice features for doing so
+  dtg::sshtunnelhost::authorizedkey{$keys:
+    username    => $username,
+    destination => $destination,
+  }
+}
+define dtg::sshtunnelhost::authorizedkey($username, $destination) {
+  $keyarray = split($title, ' ')
+  $keyname = $keyarray[0]
+  ssh_authorized_key { "${keyname},${username}":
+    type    => $keyarray[1],
+    key     => $keyarray[2],
+    user    => $username,
+    ensure  => present,
+    options => ['no-pty','no-X11-forwarding',"permitopen=\"$destination\"",'command="/bin/echo do-not-send-commands"'],
+  }
+}
