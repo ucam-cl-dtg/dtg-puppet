95d362983a3d6358e99d85c7c5424aa0f648566a
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2014-11-09 18:39:01 +0000

    Correct so-22-n name. Also corrected DHCP range.

--- manifests/nodes/dhcp.pp
+++ manifests/nodes/dhcp.pp
@@ -29,13 +29,13 @@ node 'dhcp.dtg.cl.cam.ac.uk' {
   dhcp::pool{ 'dtg.cl.cam.ac.uk':
     network => '128.232.20.0',
     mask    => '255.255.252.0',
-    range   => ['128.232.20.17', '128.232.20.59'],
+    range   => ['128.232.20.8', '128.232.20.59'],
     # gateway should be route.cl
     gateway => '128.232.20.1',
     parameters => 'deny unknown-clients',
   }
   dhcp::host {
-    'so-22-8':mac => 00:1e:67:51:c4:27'', ip => '128.232.22.8';
+    'so-22-9':mac => 00:1e:67:51:c4:27'', ip => '128.232.22.8';
     'inuit':mac => '00:92:58:00:53:c3', ip => '128.232.20.17';
     'puppy0':mac => "00:16:3E:E8:14:1C", ip => "128.232.20.28";
     'puppy1':mac => "00:16:3E:E8:14:1D", ip => "128.232.20.29";
