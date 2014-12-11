f6ba34448d804a9c8eb8741edda60bec806cb3b5
Stephan Kollmann <sak70@cam.ac.uk>
2014-12-11 11:36:32 +0000

    sak70-vpnserver: iptables allow incoming udp 500,4500

--- manifests/nodes/sak70-vpnserver.pp
+++ manifests/nodes/sak70-vpnserver.pp
@@ -2,6 +2,19 @@
 node 'sak70-vpnserver.dtg.cl.cam.ac.uk' {
   include 'dtg::minimal'
   User<|title == sak70 |> { groups +>[ 'adm' ]}
+
+  firewall { "031-vpnserver accept udp 500":
+    proto   => 'udp',
+    dport   => 500,
+    action  => 'accept',
+  }
+
+  firewall { "032-vpnserver accept udp 4500":
+    proto   => 'udp',
+    dport   => 4500,
+    action  => 'accept',
+  }
+
 }
 if ( $::monitor ) {
   nagios::monitor { 'sak70-vpnserver':
