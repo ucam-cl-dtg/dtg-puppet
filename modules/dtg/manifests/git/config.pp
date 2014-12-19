c6bf5ea1de58871cd3232785a7c5a1b669343cef
Daniel Thomas <drt24@cam.ac.uk>
2014-12-19 14:37:31 +0000

    Ensure that git fsck is also run when fetching as well as when being pushed at

--- modules/dtg/manifests/git/config.pp
+++ modules/dtg/manifests/git/config.pp
@@ -13,9 +13,17 @@ class dtg::git::config::repohost {
 
   # This verifies the SHA-1 checksums on all objects on pushes, which slows that down
   # but also prevents corruption of the repositories
-  exec{'git fsckObjects':
-    command => 'git config --system receive.fsckObjects true',
-    unless => 'git config --get receive.fsckObjects',
+  exec{'git receive.fsckobjects':
+    command => 'git config --system receive.fsckobjects true',
+    unless => 'git config --get receive.fsckobjects',
+  }
+  exec{'git transfer.fsckobjects':
+    command => 'git config --system transfer.fsckobjects true',
+    unless => 'git config --get transfer.fsckobjects',
+  }
+  exec{'git fetch.fsckobjects':
+    command => 'git config --system fetch.fsckobjects true',
+    unless => 'git config --get fetch.fsckobjects',
   }
 }
 # $name is the username, $real_name is the name the user is generally known by
