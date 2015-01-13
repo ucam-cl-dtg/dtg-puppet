f497a72c6f8a8b5d17b77cbef59480abc2ce6216
Daniel Thomas <drt24@cam.ac.uk>
2015-01-13 13:15:16 +0000

    We want the mlocate not the locate package as mlocate provides a better implementation of locate

--- modules/dtg/manifests/minimal.pp
+++ modules/dtg/manifests/minimal.pp
@@ -16,7 +16,7 @@ class dtg::minimal ($manageapt = true, $adm_sudoers = true) {
   # Packages which should be installed on all servers
   $packagelist = ['traceroute', 'vim', 'screen', 'fail2ban', 'curl', 'tar',
                   'runit', 'apg', 'emacs24-nox', 'htop', 'nfs-common',
-                  'iptables-persistent', 'command-not-found', 'locate',
+                  'iptables-persistent', 'command-not-found', 'mlocate',
                   'bash-completion']
   package {
     $packagelist:
@@ -24,7 +24,7 @@ class dtg::minimal ($manageapt = true, $adm_sudoers = true) {
   }
 
   # Packages that should not be installed on a server
-  $banned_packages = ['sl', 'emacs23-common', 'emacs23-bin-common', 'whoopsie']
+  $banned_packages = ['sl', 'emacs23-common', 'emacs23-bin-common', 'whoopsie', 'locate']
 
   package {
     $banned_packages:
