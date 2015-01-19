0167475d45369f705c06a16609dd0446067f7b9b
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2015-01-19 18:34:21 +0000

    Xmonad on spaniel

--- manifests/nodes/spaniel.pp
+++ manifests/nodes/spaniel.pp
@@ -2,4 +2,9 @@ node 'entropy.dtg.cl.cam.ac.uk' {
   # We don't have a local mirror of raspbian to point at
   class { 'dtg::minimal': manageapt => false, }
 
+  $packages = ['xmonad']
+  package{$packages:
+    ensure => installed
+  }
+
 }
