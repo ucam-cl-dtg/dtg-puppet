532dfd0020ccc8793b5d6fd825ba40af226e464b
Daniel Thomas <drt24@cam.ac.uk>
2015-01-20 12:58:26 +0000

    Install the kernel if it has gone missing!

--- modules/dtg/manifests/minimal.pp
+++ modules/dtg/manifests/minimal.pp
@@ -17,7 +17,7 @@ class dtg::minimal ($manageapt = true, $adm_sudoers = true) {
   $packagelist = ['traceroute', 'vim', 'screen', 'fail2ban', 'curl', 'tar',
                   'runit', 'apg', 'emacs24-nox', 'htop', 'nfs-common',
                   'iptables-persistent', 'command-not-found', 'mlocate',
-                  'bash-completion']
+                  'bash-completion', 'linux-image-generic']
   package {
     $packagelist:
       ensure => installed
