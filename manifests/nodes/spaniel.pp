3dd81b1c789a31344562305a64f16678983b4775
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2015-01-20 16:31:02 +0000

    Monitor spaniel.

--- manifests/nodes/spaniel.pp
+++ manifests/nodes/spaniel.pp
@@ -2,9 +2,17 @@ node 'spaniel.dtg.cl.cam.ac.uk' {
   # We don't have a local mirror of raspbian to point at
   class { 'dtg::minimal': manageapt => false, }
 
-  $packages = ['xmonad']
+  $packages = ['xmonad', 'x11-xserver-utils', 'unclutter']
   package{$packages:
     ensure => installed
   }
+}
 
+if ( $::monitor ) {
+  nagios::monitor { 'spaniel':
+    parents    => '',
+    address    => 'spaniel.dtg.cl.cam.ac.uk',
+    hostgroups => [ 'ssh-servers' ],
+  }
+  munin::gatherer::configure_node { 'spaniel': }
 }
