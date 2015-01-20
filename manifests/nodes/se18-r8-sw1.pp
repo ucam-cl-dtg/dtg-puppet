aad1cbbdfe9b124e509cc32381efb4ec1f699102
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2015-01-20 09:54:52 +0000

    Monitor our ToR switch with nagios.

---
+++ manifests/nodes/se18-r8-sw1.pp
@@ -0,0 +1,8 @@
+# Monitor CL infrastructure so that we know when it is down
+if ( $::monitor ) {
+  nagios::monitor { 'se18-r8-sw1':
+    parents    => '',
+    address    => 'se18-r8-sw1.net.cl.cam.ac.uk',
+    hostgroups => [ 'http-servers', 'https-servers' ],
+  }
+}
