0d5cd4b5c9a3c343f6e5c2c3dfcb413e920905c2
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2015-01-22 17:49:27 +0000

    Open ports 80 and 8080 on so-22-8,9

--- manifests/nodes/so-22-8.pp
+++ manifests/nodes/so-22-8.pp
@@ -1,3 +1,11 @@
+node 'so-22-8.dtg.cl.cam.ac.uk' {
+  include 'dtg::minimal'
+
+  class { 'dtg::firewall::publichttp': }
+  ->
+  class { 'dtg::firewall::80to8080': }
+}
+
 if ( $::monitor ) {
   munin::gatherer::configure_node { 'so-22-8': }
 }
--- manifests/nodes/so-22-9.pp
+++ manifests/nodes/so-22-9.pp
@@ -1,3 +1,15 @@
+node 'so-22-9.dtg.cl.cam.ac.uk' {
+  include 'dtg::minimal'
+
+  class { 'dtg::firewall::publichttp': }
+
+  firewall { 'Accept 8080':
+    proto  => 'tcp',
+    dport  => '8080',
+    action => 'accept',
+  }
+}
+
 if ( $::monitor ) {
   munin::gatherer::configure_node { 'so-22-9': }
 }
