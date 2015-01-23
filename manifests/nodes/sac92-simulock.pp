8509bdb89b4013e12f49569dd84952705bea6539
Stephen Cummins <sacummins@gmail.com>
2015-01-23 13:26:07 +0000

    Changed ssh keys

--- manifests/nodes/sac92-simulock.pp
+++ manifests/nodes/sac92-simulock.pp
@@ -9,13 +9,13 @@ node 'sac92-simulock.dtg.cl.cam.ac.uk' {
   } ->
   ssh_authorized_key {'rsa33_key_windows':
     ensure => present,
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDPBjD5pcqSSszBGhWnjt5Na8+JBZQWcE0z0qF4BIaynlCUy1ATe6fopo3lA0+zOuUXMbRb/uh6aUkD3KTvSRLZ9IVNmlgeDCuXNQRiznsERaYVX7Vq/ixvU5YoCmhE7CvYNm4SXsHEO8HS1+IhBXwNg8XHWRNxdO0RxvheC1obGYJwQpN062jCCOPJ2HqUJoKJ1jcnIIzELS1+6yl2bQvslSJ3CxGuTypXOCCLELAUA2Hzl9xr2IShXjf8qsWFxQdv948NM/SxYcwy+9W8TOTzI26RKdXGrcetdUBV9YNXkTSUD35qJgdQEiVn0Z0nwYm33JW45PhiXkZsrdUseUDT Richard@Sonic',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDPBjD5pcqSSszBGhWnjt5Na8+JBZQWcE0z0qF4BIaynlCUy1ATe6fopo3lA0+zOuUXMbRb/uh6aUkD3KTvSRLZ9IVNmlgeDCuXNQRiznsERaYVX7Vq/ixvU5YoCmhE7CvYNm4SXsHEO8HS1+IhBXwNg8XHWRNxdO0RxvheC1obGYJwQpN062jCCOPJ2HqUJoKJ1jcnIIzELS1+6yl2bQvslSJ3CxGuTypXOCCLELAUA2Hzl9xr2IShXjf8qsWFxQdv948NM/SxYcwy+9W8TOTzI26RKdXGrcetdUBV9YNXkTSUD35qJgdQEiVn0Z0nwYm33JW45PhiXkZsrdUseUDT',
     user   => 'rsa33',
     type   => 'ssh-rsa',
-  }
+  }->
   ssh_authorized_key {'rsa33_key_ubuntu':
     ensure => present,
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDx92pV9kH5JlPT/1KDUJojGUWgdgxZgQ6vBhmyEYpkrHg8cttz2d2Y4rFt/T6Fw2khvyFtSu4/xPKFzFrnnQ8E3Rd7PGot4u5hNGHMo5W9MTp+3aV0913s9cu824+FdnXJvuGewX/DEDbcsAwAqBg5n+8yjLUs0Szm5OccQTNJWMXY9lunITmZevIxABgEfv6aJZEoIbqnVOg+BsoFMEqD4Wzf5esSztE+rzIH3jFPg1FybWLsmupbt/FMFUtyQg3Q4YMjhZ4yR2M+ty8KDrdDPaim5OnU5sqFK/GVY6K9sqbaOcxEihUvBtviWY4I5j8LrGcehFX5Pj5CbYXQcol7 richard@shadow',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDx92pV9kH5JlPT/1KDUJojGUWgdgxZgQ6vBhmyEYpkrHg8cttz2d2Y4rFt/T6Fw2khvyFtSu4/xPKFzFrnnQ8E3Rd7PGot4u5hNGHMo5W9MTp+3aV0913s9cu824+FdnXJvuGewX/DEDbcsAwAqBg5n+8yjLUs0Szm5OccQTNJWMXY9lunITmZevIxABgEfv6aJZEoIbqnVOg+BsoFMEqD4Wzf5esSztE+rzIH3jFPg1FybWLsmupbt/FMFUtyQg3Q4YMjhZ4yR2M+ty8KDrdDPaim5OnU5sqFK/GVY6K9sqbaOcxEihUvBtviWY4I5j8LrGcehFX5Pj5CbYXQcol7',
     user   => 'rsa33',
     type   => 'ssh-rsa',
   }
