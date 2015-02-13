93cb5934df1f1218178c997c880aac560268395b
Stephen Cummins <sacummins@gmail.com>
2015-02-13 08:21:42 +0000

    added varnish to cdn config

--- manifests/nodes/cdn.pp
+++ manifests/nodes/cdn.pp
@@ -1,12 +1,49 @@
 node 'cdn.dtg.cl.cam.ac.uk' {
   include 'dtg::minimal'
+
+  $apache_http_port = '8080'
   
   class {'apache::ubuntu': } ->
   apache::module {'cgi':} ->
   apache::module {'headers':} ->
   apache::module {'rewrite':} ->
-  apache::module {'expires':}
+  apache::module {'expires':} ->
+  file_line{'apache-port-configure-http':
+    line   => "Listen ${apache_http_port}",
+    path   => "/etc/apache2/ports.conf",
+    match  => 'Listen 80'
+  } ->
+  file_line{'apache-port-configure-http-virtual-directory':
+    line   => "<VirtualHost *:${apache_http_port}>",
+    path   => "/etc/apache2/sites-available/000-default",
+    notify => Service["apache2"],
+    match  => '<VirtualHost \*:.*>'
+  }
   
+  $packages = ['varnish']
+  package{$packages:
+    ensure => installed
+  }
+  ->
+  service { "varnish":
+      ensure  => "running",
+      enable  => "true",
+      require => Package["varnish"],
+  }
+
+  file_line{'varnish-setup-backend':
+    line   => "port = '${apache_http_port}'",
+    path   => "/etc/varnish/default.vcl",
+    match  => "^port.*"
+  }
+  ->
+  file_line{'varnish-setup-http-listening-port':
+    notify => Service["varnish"],
+    line   => "DAEMON_OPTS=\"-a :8080 \\",
+    path   => "/etc/default/varnish",
+    match  => "^DAEMON_OPTS=.*"
+  }
+
   class {'dtg::firewall::publichttp':}
 }
 
