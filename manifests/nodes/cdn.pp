3a9ec9b8b3fdfbf6008880100203a634ba1b3821
Stephen Cummins <sacummins@gmail.com>
2015-02-13 08:56:23 +0000

    Trying to fix ordering of install operations on cdn server.

--- manifests/nodes/cdn.pp
+++ manifests/nodes/cdn.pp
@@ -3,7 +3,8 @@ node 'cdn.dtg.cl.cam.ac.uk' {
 
   $apache_http_port = '8080'
   $varnish_http_port = '80'
-  
+  $packages = ['varnish']
+
   class {'apache::ubuntu': } ->
   apache::module {'cgi':} ->
   apache::module {'headers':} ->
@@ -12,16 +13,20 @@ node 'cdn.dtg.cl.cam.ac.uk' {
   file_line{'apache-port-configure-http':
     line   => "Listen ${apache_http_port}",
     path   => "/etc/apache2/ports.conf",
-    match  => 'Listen 80'
-  } ->
+    match  => '^Listen 80.*$'
+  } 
+  ->
   file_line{'apache-port-configure-http-virtual-directory':
     line   => "<VirtualHost *:${apache_http_port}>",
-    path   => "/etc/apache2/sites-available/000-default",
+    path   => "/etc/apache2/sites-available/000-default.conf",
     notify => Service["apache2"],
     match  => '<VirtualHost \*:.*>'
+  } 
+  ->
+  exec { 'stop-apache':
+    command  => 'service apache2 stop'
   }
-  
-  $packages = ['varnish']
+  ->
   package{$packages:
     ensure => installed
   }
@@ -33,16 +38,21 @@ node 'cdn.dtg.cl.cam.ac.uk' {
   }
 
   file_line{'varnish-setup-backend':
-    line   => ".port = \"${apache_http_port}\"",
+    line   => ".port = \"${apache_http_port}\";",
     path   => "/etc/varnish/default.vcl",
-    match  => "\.port.*"
+    match  => '^\.port.*'
   }
   ->
   file_line{'varnish-setup-http-listening-port':
     notify => Service["varnish"],
-    line   => "DAEMON_OPTS=\"-a :8080 \\",
+    line   => "DAEMON_OPTS=\"-a :${varnish_http_port} \\",
     path   => "/etc/default/varnish",
     match  => "^DAEMON_OPTS=.*"
+  }
+  ->
+  exec { 'start-apache':
+    command  => 'service apache2 start',
+    refreshonly => true
   }
 
   class {'dtg::firewall::publichttp':}
