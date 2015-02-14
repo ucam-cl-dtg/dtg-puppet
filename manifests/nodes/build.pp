d1018526a250afb5af371b66fe4c3677271631a0
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2015-02-14 20:53:00 +0000

    Open firewall ports for distcc.

--- manifests/nodes/build.pp
+++ manifests/nodes/build.pp
@@ -2,6 +2,13 @@ node /build-(\d+)?/ {
   include 'dtg::minimal'
   class {'distcc':
     listen_ip_range => $dtg_subnet,
-    listen_on_ip    => $::ipaddress,
+    listen_on_ip    => "",
+  }
+
+
+  firewall { "031-build accept tcp 3632":
+    proto   => 'tcp',
+    dport   => 3632,
+    action  => 'accept',
   }
 }
