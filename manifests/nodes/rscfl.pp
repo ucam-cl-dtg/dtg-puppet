83d2ec2c7ecc4bc64ced31d4046146cc8ec1788b
Oliver Chick <oc243@cam.ac.uk>
2015-02-18 09:45:11 +0000

    Run avahi on rscfl machines for distcc.

--- manifests/nodes/rscfl.pp
+++ manifests/nodes/rscfl.pp
@@ -4,4 +4,10 @@ node /.*rscfl.*/ {
     listen_ip_range => $dtg_subnet,
     listen_on_ip    => '',
   }
+  firewall { "032-build accept avahi udp 5353":
+    proto   => 'udp',
+    dport   => 5353,
+    action  => 'accept',
+  }
+
 }
