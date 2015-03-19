7fd256d3070df4993c8538152649ba8c494f1bd8
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2015-03-19 13:51:34 +0000

    Don't monitor http on rscfl-demo.
    It isn't quite production ready yet.

--- manifests/nodes/rscfl-demo.pp
+++ manifests/nodes/rscfl-demo.pp
@@ -10,7 +10,7 @@ if ( $::monitor ) {
   nagios::monitor { 'rscfl-demo':
     parents    => 'nas04',
     address    => 'rscfl-demo.dtg.cl.cam.ac.uk',
-    hostgroups => [ 'ssh-servers', 'http-servers' ],
+    hostgroups => [ 'ssh-servers' ],
   }
   munin::gatherer::configure_node { 'rscfl-demo': }
 }
