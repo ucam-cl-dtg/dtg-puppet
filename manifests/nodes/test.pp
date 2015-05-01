f42d4e85b78a051ff7e7af1ee8ea2e09f7d5366c
Daniel Thomas <drt24@cam.ac.uk>
2015-05-01 08:35:25 +0100

    Add Sherif as an admin of test-puppet

--- manifests/nodes/test.pp
+++ manifests/nodes/test.pp
@@ -1,5 +1,6 @@
 node 'test-puppet.dtg.cl.cam.ac.uk' {
   include 'dtg::minimal'
+  User<|title == sa497 |> { groups +>[ 'adm' ]}
 }
 if ( $::monitor ) {
   munin::gatherer::configure_node { 'test-puppet': }
