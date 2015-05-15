72b7bf3be2f36f42697f6652ed7b127f430a4627
Stephan Kollmann <stephan.kollmann--git@cl.cam.ac.uk>
2015-05-15 11:27:54 +0100

    removed sak70-vpnserver from nagios

--- manifests/nodes/sak70-vpnserver.pp
+++ manifests/nodes/sak70-vpnserver.pp
@@ -35,10 +35,10 @@ node 'sak70-vpnserver.dtg.cl.cam.ac.uk' {
 
 }
 if ( $::monitor ) {
-  nagios::monitor { 'sak70-vpnserver':
-    parents    => 'nas04',
-    address    => 'sak70-vpnserver.dtg.cl.cam.ac.uk',
-    hostgroups => [ 'ssh-servers' ],
-  }
+#  nagios::monitor { 'sak70-vpnserver':
+#    parents    => 'nas04',
+#    address    => 'sak70-vpnserver.dtg.cl.cam.ac.uk',
+#    hostgroups => [ 'ssh-servers' ],
+#  }
   munin::gatherer::configure_node { 'sak70-vpnserver': }
 }
