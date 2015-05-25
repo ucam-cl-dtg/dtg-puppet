efa2ac8f73e6ee42f36b17527b6d3245fc3ffe87
David Turner <david.w.turner@cl.cam.ac.uk>
2015-05-25 18:18:49 +0100

    Shifted files to puppet resources, added venv run

--- manifests/nodes/dwt27-crunch.pp
+++ manifests/nodes/dwt27-crunch.pp
@@ -9,10 +9,36 @@ node 'dwt27-crunch.dtg.cl.cam.ac.uk' {
                   'libfftw3-3',
                   'liblapack3gf', 'liblapack-dev',
                   'libblas3gf', 'libblas-dev',
-                  'python-zmq']
+                  'python-zmq',]
   package {
     $packagelist:
         ensure => present
+  } ->
+  file { '/home/dwt27/ipc':
+    ensure => directory,
+    owner => 'dwt27',
+    group => 'dwt27',
+    mode => '0775',
+  } ->
+  file { '/home/dwt27/ipc/requirements.txt':
+    ensure => file,
+    owner => 'dwt27',
+    group => 'dwt27',
+    mode => '0664',
+    source => 'puppet:///modules/dtg/dwt27-crunch/requirements.txt',
+  } ->
+  file { '/home/dwt27/ipc/setup_venv.sh',
+    ensure => file,
+    owner => 'dwt27',
+    group => 'dwt27',
+    mode => '0775',
+    source => 'puppet:///modules/dtg/dwt27-crunch/setup_venv.sh',
+  } ->
+  exec { 'setup_venv':
+    command => '/home/dwt27/ipc/setup_venv.sh',
+    cwd => '/home/dwt27/ipc/',
+    user => 'dwt27',
+    group => 'dwt27',
+    creates => '/home/dwt27/ipc/venv'
   }
-
 }
