7a14b12bbb821d5a5f71f5be317a6ab73b782deb
David Turner <david.w.turner@cl.cam.ac.uk>
2015-05-30 18:41:26 +0100

    Added ipython cluster profile to dwt27-crunch

--- modules/dtg/files/dwt27-crunch/start_ipc.sh
+++ modules/dtg/files/dwt27-crunch/start_ipc.sh
@@ -5,5 +5,6 @@ ps xc | grep -F ipcluster >/dev/null && exit 0
 
 cd /home/dwt27/ipc
 source venv/bin/activate
-ipcluster start -n 7 >/dev/null 2>&1
+cd /home/dwt27/nas04/ipc
+ipcluster start --profile=crunch -n 7 >/dev/null 2>&1
 echo Started/restarted ipcluster at `date`
