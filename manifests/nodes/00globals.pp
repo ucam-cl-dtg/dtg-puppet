0a5b32844e8dc4ab9ee139a6867576480766f3e4
Daniel Thomas <drt24@cam.ac.uk>
2015-06-12 10:13:11 +0100

    Override default service provider to systemd on ubuntu 15.04
    Waiting until https://github.com/puppetlabs/puppet/pull/3932 hits Ubuntu

--- manifests/nodes/00globals.pp
+++ manifests/nodes/00globals.pp
@@ -205,3 +205,13 @@ if ( $::fqdn in $backup_hosts ) {
 }
 
 $nfs_client_port = 1025
+
+
+
+# Override Service definition until we are on a new enough puppet to know about ubuntu and systemd
+
+if $::operatingsystem == 'Ubuntu' and $::operatingsystemmajrelease == "15.04" {
+  Service {
+    provider => systemd,
+  }
+}
