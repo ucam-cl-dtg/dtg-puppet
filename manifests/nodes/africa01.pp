804c34c7c1dc166e60bafa9dd11fa10afbd67543
Sherif Akoush <sa497@cam.ac.uk>
2015-06-12 11:09:48 +0100

    africa01.pp and bugs

--- manifests/nodes/africa01.pp
+++ manifests/nodes/africa01.pp
@@ -1,7 +1,17 @@
-node 'africa01.cl.cam.ac.uk' {      
-  
+node 'africa01.cl.cam.ac.uk' {
+  include 'dtg::minimal'
   include 'nfs::server'  
 
   class {'dtg::zfs': }
 
 }
+
+if ( $::monitor ) {
+  nagios::monitor { 'africa01':
+    parents    => '',
+    address    => 'africa01.cl.cam.ac.uk',
+    hostgroups => [ 'ssh-servers' ],
+  }
+  munin::gatherer::configure_node { 'africa01': }
+}
+
