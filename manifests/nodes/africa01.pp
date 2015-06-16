d4638b0e9212f1004693249cc7ad615fc1755797
Sherif Akoush <sakoush@c141.al.cl.cam.ac.uk>
2015-06-16 12:51:33 +0100

    open ports for hadoop

--- manifests/nodes/africa01.pp
+++ manifests/nodes/africa01.pp
@@ -14,65 +14,65 @@ node 'africa01.cl.cam.ac.uk' {
     weekday => 1,
   }
 
-  $portmapper_port     = 111
-  $nfs_port            = 2049
-  $lockd_tcpport       = 32803
-  $lockd_udpport       = 32769
-  $mountd_port         = 892
-  $rquotad_port        = 875
-  $statd_port          = 662
-  $statd_outgoing_port = 2020
+#$portmapper_port     = 111
+#$nfs_port            = 2049
+#$lockd_tcpport       = 32803
+#$lockd_udpport       = 32769
+#$mountd_port         = 892
+#$rquotad_port        = 875
+#$statd_port          = 662
+#$statd_outgoing_port = 2020
 
-  augeas { 'nfs-kernel-server':
-    context => '/files/etc/default/nfs-kernel-server',
-    changes => [
-                "set LOCKD_TCPPORT ${lockd_tcpport}",
-                "set LOCKD_UDPPORT ${lockd_udpport}",
-                "set MOUNTD_PORT ${mountd_port}",
-                "set RQUOTAD_PORT ${rquotad_port}",
-                "set STATD_PORT ${statd_port}",
-                "set STATD_OUTGOING_PORT ${statd_outgoing_port}",
-                "set RPCMOUNTDOPTS \"'--manage-gids --port ${mountd_port}'\"",
-                ],
-    notify  => Service['nfs-kernel-server']
-  }
-  dtg::firewall::nfs {'nfs access from dtg':
-    source          => '128.232.20.0/22',
-    source_name     => 'dtg',
-    portmapper_port => $portmapper_port,
-    nfs_port        => $nfs_port,
-    lockd_tcpport   => $lockd_tcpport,
-    lockd_udpport   => $lockd_udpport,
-    mountd_port     => $mountd_port,
-    rquotad_port    => $rquotad_port,
-    statd_port      => $statd_port,
-  }
+#augeas { 'nfs-kernel-server':
+#context => '/files/etc/default/nfs-kernel-server',
+#changes => [
+#"set LOCKD_TCPPORT ${lockd_tcpport}",
+#"set LOCKD_UDPPORT ${lockd_udpport}",
+#"set MOUNTD_PORT ${mountd_port}",
+#"set RQUOTAD_PORT ${rquotad_port}",
+#"set STATD_PORT ${statd_port}",
+#"set STATD_OUTGOING_PORT ${statd_outgoing_port}",
+#"set RPCMOUNTDOPTS \"'--manage-gids --port ${mountd_port}'\"",
+#],
+#notify  => Service['nfs-kernel-server']
+#}
+#  dtg::firewall::nfs {'nfs access from dtg':
+#    source          => '128.232.20.0/22',
+#    source_name     => 'dtg',
+#    portmapper_port => $portmapper_port,
+#    nfs_port        => $nfs_port,
+#    lockd_tcpport   => $lockd_tcpport,
+#    lockd_udpport   => $lockd_udpport,
+#    mountd_port     => $mountd_port,
+#    rquotad_port    => $rquotad_port,
+#    statd_port      => $statd_port,
+#  }
 
-  firewall { '050 accept all sa497-crunch-0.dtg.cl.cam.ac.uk':
+  firewall { '001 accept all sa497-crunch-0.dtg.cl.cam.ac.uk':
     action => 'accept',
     source => 'sa497-crunch-0.dtg.cl.cam.ac.uk'
   }
-  firewall { '050 accept all sa497-crunch-1.dtg.cl.cam.ac.uk':
+  firewall { '001 accept all sa497-crunch-1.dtg.cl.cam.ac.uk':
     action => 'accept',
     source => 'sa497-crunch-1.dtg.cl.cam.ac.uk'
   }
-  firewall { '050 accept all sa497-crunch-2.dtg.cl.cam.ac.uk':
+  firewall { '001 accept all sa497-crunch-2.dtg.cl.cam.ac.uk':
     action => 'accept',
     source => 'sa497-crunch-2.dtg.cl.cam.ac.uk'
   }
-  firewall { '050 accept all sa497-crunch-3.dtg.cl.cam.ac.uk':
+  firewall { '001 accept all sa497-crunch-3.dtg.cl.cam.ac.uk':
     action => 'accept',
     source => 'sa497-crunch-3.dtg.cl.cam.ac.uk'
   }
-  firewall { '050 accept all vm-sr-nile0.cl.cam.ac.uk':
+  firewall { '001 accept all vm-sr-nile0.cl.cam.ac.uk':
     action => 'accept',
     source => 'vm-sr-nile0.cl.cam.ac.uk',
   }
-  firewall { '050 accept all vm-sr-nile1.cl.cam.ac.uk':
+  firewall { '001 accept all vm-sr-nile1.cl.cam.ac.uk':
     action => 'accept',
     source => 'vm-sr-nile1.cl.cam.ac.uk',
   }
-  firewall { '050 accept all vm-sr-nile2.cl.cam.ac.uk':
+  firewall { '001 accept all vm-sr-nile2.cl.cam.ac.uk':
     action => 'accept',
     source => 'vm-sr-nile2.cl.cam.ac.uk',
   }
