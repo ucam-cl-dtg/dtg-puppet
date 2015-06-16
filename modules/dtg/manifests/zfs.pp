846edfaafa5c5125e233746afc4d04c31022b333
Sherif Akoush <sakoush@c141.al.cl.cam.ac.uk>
2015-06-16 11:22:49 +0100

    require linux headers

--- modules/dtg/manifests/zfs.pp
+++ modules/dtg/manifests/zfs.pp
@@ -2,7 +2,7 @@ class dtg::zfs {
   class {'dtg::zfs::repos': stage => 'repos'}
   package {'ubuntu-zfs':
     ensure  => present,
-    require => [ Apt::Ppa['ppa:zfs-native/stable'], Package['munin-node']],
+    require => [ Package["linux-headers-$kernelrelease"], Apt::Ppa['ppa:zfs-native/stable'], Package['munin-node']],
   }
 
   # zfs includes this config file to let unpriviliged users run read only ZFS commands.
