ec0cf2fc78b2531e50791f46b9abd02372345325
Sherif Akoush <sakoush@c105.al.cl.cam.ac.uk>
2015-06-17 17:36:38 +0100

    adding firewall rules and packages

--- manifests/nodes/sa497-crunch.pp
+++ manifests/nodes/sa497-crunch.pp
@@ -1,6 +1,54 @@
 node /sa497-crunch(-\d+)?/ {
     include 'dtg::minimal'
 
-  User<|title == sa497 |> { groups +>[ 'adm' ]}
+    User<|title == sa497 |> { groups +>[ 'adm' ]}
+
+    firewall { '001 accept all sa497-crunch-0.dtg.cl.cam.ac.uk':
+        action => 'accept',
+        source => 'sa497-crunch-0.dtg.cl.cam.ac.uk'
+    }
+
+    firewall { '001 accept all sa497-crunch-1.dtg.cl.cam.ac.uk':
+        action => 'accept',
+        source => 'sa497-crunch-1.dtg.cl.cam.ac.uk'
+    }
+
+    firewall { '001 accept all sa497-crunch-2.dtg.cl.cam.ac.uk':
+        action => 'accept',
+        source => 'sa497-crunch-2.dtg.cl.cam.ac.uk'
+    }
+
+    firewall { '001 accept all sa497-crunch-3.dtg.cl.cam.ac.uk':
+        action => 'accept',
+        source => 'sa497-crunch-3.dtg.cl.cam.ac.uk'
+    }
+
+    firewall { '001 accept all vm-sr-nile0.cl.cam.ac.uk':
+        action => 'accept',
+        source => 'vm-sr-nile0.cl.cam.ac.uk',
+    }
+
+    firewall { '001 accept all vm-sr-nile1.cl.cam.ac.uk':
+        action => 'accept',
+        source => 'vm-sr-nile1.cl.cam.ac.uk',
+    }
+
+    firewall { '001 accept all vm-sr-nile2.cl.cam.ac.uk':
+        action => 'accept',
+        source => 'vm-sr-nile2.cl.cam.ac.uk',
+    }
+
+    #not sure why it doesnt accept anya.ad.cl.cam.ac.uk by name
+    firewall { '001 accept all 128.232.29.5':
+        action => 'accept',
+        source => '128.232.29.5',
+    }
+
+    $packagelist = [ 'bison' , 'flex', 'autoconf' , 'pkg-config' , 'libglib2.0-dev', 'libpcap-dev' , 'mountall' , ' liblz4-tool']
+    package {
+        $packagelist:
+        ensure => installed
+    }
+
 
 }
