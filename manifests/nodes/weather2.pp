a87f4c3022f9287b57df305d2f4f9111399fd5ac
David Turner <david.w.turner@cl.cam.ac.uk>
2015-06-25 19:42:46 +0100

    weather2: puppetise interfaces, install nginx

--- manifests/nodes/weather2.pp
+++ manifests/nodes/weather2.pp
@@ -3,9 +3,31 @@ $weather2_ip = $weather2_ips[0]
 
 node 'weather2.dtg.cl.cam.ac.uk' {
   class { 'dtg::minimal': }
-  class {'dtg::firewall::publichttp':}
+  class {'dtg::firewall::publichttp':}  # Allow port 80 incoming
 
+  # Make dwt27 have admin on this machine
   User<|title == 'dwt27' |> { groups +>[ 'adm' ]}
+
+  # Install our interfaces file with weather2's static IP:
+  file {'/etc/network/interfaces':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/dtg/weather2/interfaces',
+  }
+
+  # Install all our packages
+  $packagelist = ['nginx',]
+  package {
+      $packagelist:
+        ensure => installed
+  }
+  # Ensure nginx starts on boot
+  service {"nginx"
+    enable => true,
+  }
+
 }
 
 # Disable monitoring until things are more stable:
