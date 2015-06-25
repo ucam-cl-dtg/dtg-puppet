2e1b0c7ebdfd50ecb011a602aa5d209b9a568b6a
David Turner <david.w.turner@cl.cam.ac.uk>
2015-06-26 00:51:22 +0100

    weather2: Install and setup gunicorn and stuff

--- manifests/nodes/weather2.pp
+++ manifests/nodes/weather2.pp
@@ -19,13 +19,13 @@ node 'weather2.dtg.cl.cam.ac.uk' {
 
   # Install all our packages
   $packagelist = ['nginx', 'python3', 'python3-dev', 'python3-pip',
-                  'python3-virtualenv',]
+                  'python-virtualenv', 'python3-virtualenv',]
   package {$packagelist:
         ensure => installed,
-  }
-  # Ensure nginx starts on boot
+  } -> # Ensure nginx starts on boot
   service {"nginx":
     enable => true,
+    ensure => running,
   }
 
   # Setup our weather webapp user
@@ -39,7 +39,48 @@ node 'weather2.dtg.cl.cam.ac.uk' {
     password => '*',
     managehome => true,
     gid => 'weather',
-    require => Group['weather'],
+  }
+
+  # Retrieve the weather server git repository:
+  vcsrepo {'/srv/weather/weather-srv-2':
+    ensure => latest,
+    provider => git,
+    # TODO: move the repository to the ucam-cl-dtg organization
+    source => 'https://github.com/cillian64/dtg-weather-2.git',
+    revision => 'master',
+    user => 'weather',
+    require => User['weather'],
+    notify => Service['weather-service'],  # Restart service upon changes
+  }
+  
+  # Setup the venv and stuff if required:
+  exec {'create-venv':
+    creates => '/srv/weather/venv',
+    command => '/srv/weather/weather-srv-2/create_venv.sh',
+    cwd => '/srv/weather/',
+    user => 'weather',
+    group => 'weather',
+    require => Vcsrepo['/srv/weather/weather-srv-2'],
+  }
+  
+  # Install the upstart script
+  file {'upstart-script':
+    path => '/etc/init/weather.conf',
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    source => 'file:///srv/weather/weather-srv-2/upstart_script.conf',
+    require => Vcsrepo['/srv/weather/weather-srv-2'],
+  }
+  
+  # Start up the weather service!
+  service {'weather-service':
+    name => 'weather',
+    ensure => running,
+    enable => true,
+    require => Exec['create-venv'],
+    require => File['upstart-script'],
+    require => Vcsrepo['/srv/weather/weather-srv-2'],
   }
 }
 
