799c5aaee33527f4662426a720a8c79a13e4d846
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2015-09-17 20:04:24 +0100

    cleanup redundant if checks for wolf machines.

--- manifests/nodes/wolf0.pp
+++ manifests/nodes/wolf0.pp
@@ -6,12 +6,9 @@ node /wolf(\d+)?/ {
 
 if ( $::monitor ) {
   munin::gatherer::configure_node { 'wolf0': }
-}
-if ( $::monitor ) {
   munin::gatherer::configure_node { 'wolf1': }
-}
-if ( $::monitor ) {
   munin::gatherer::configure_node { 'wolf2': }
+
   nagios::monitor { 'wolf2-bmc':
     parents    => '',
     address    => 'wolf2-bmc.dtg.cl.cam.ac.uk',
