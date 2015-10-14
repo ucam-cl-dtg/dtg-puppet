a29a77055d6b3d49e07179fa19fc3470cace5202
Ian Davies <ipd21@cam.ac.uk>
2015-10-14 13:15:36 +0100

    Update Isaac ticket server config.

---
+++ manifests/nodes/isaac-tickets.pp
@@ -0,0 +1,63 @@
+node 'isaac-tickets.dtg.cl.cam.ac.uk' {
+  include 'dtg::minimal'
+
+  class {'dtg::firewall::publichttp':}
+  class {'dtg::firewall::publichttps':}
+
+  $packages = ['rssh', 'inotify-tools',  'php5', 'libapache2-mod-php5', 'php5-mcrypt', 'zip', 'unzip', 'php5-mysql', 'php5-imap', 'php5-gd']
+  package{$packages:
+    ensure => installed
+  }
+  ->
+  file_line { 'rssh-allow-sftp':
+    line => 'allowsftp',
+    path => '/etc/rssh.conf',
+  } 
+  ->
+  class {'apache::ubuntu': } ->
+  class {'dtg::apache::raven': server_description => 'Isaac Physics'} ->
+  apache::module {'cgi':} ->
+  apache::module {'headers':} ->
+  apache::module {'rewrite':} ->
+  apache::module {'proxy':} ->
+  apache::module {'proxy_http':} ->
+  apache::module {'ssl':} ->
+  apache::site {'isaac-tickets':
+    source => 'puppet:///modules/dtg/apache/isaac-tickets.conf',
+  } ->
+  file { '/var/www-osticket':
+    ensure => 'directory',
+    owner  => 'www-data',
+    group  => 'www-data',
+    mode   => '0755',
+  }
+  ->
+  exec { 'enable php imap':
+    command => 'sudo php5enmod imap',
+  }
+
+  cron { 'osticket-cron':
+    command => '/usr/bin/php /var/www-osticket/upload/api/cron.php',
+    user    => www-data,
+    minute  => '*/1'
+  }
+
+
+}
+
+if ( $::monitor ) {
+  nagios::monitor { 'isaac-tickets':
+    parents    => 'nas04',
+    address    => 'isaac-tickets.dtg.cl.cam.ac.uk',
+    hostgroups => [ 'ssh-servers', 'http-servers', 'https-servers'],
+  }
+  munin::gatherer::configure_node { 'isaac-tickets': }
+}
+
+
+## For a new server, unpack osTicket.zip into /var/www-osticket, following instructions at http://osticket.com/wiki/Installation
+## We also need MySQL:
+##
+##    sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+##    sudo mysql_install_db
+##    sudo /usr/bin/mysql_secure_installation
