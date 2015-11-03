a342e6e9adb2ed36ab942ced7f685cd354e68476
madgen <me@madgen.net>
2015-11-03 13:03:04 +0000

    Add missing liblapack dependency for CamFort

--- modules/dtg/manifests/jenkins.pp
+++ modules/dtg/manifests/jenkins.pp
@@ -22,7 +22,7 @@ class dtg::jenkins {
     'jenkins-crypto-util', 'jenkins-external-job-monitor', 'jenkins-instance-identity', 'jenkins-memory-monitor', 'jenkins-ssh-cli-auth',
     'python3-markdown', 'mercurial', 'python3-urllib3', 'python3-dateutil', 'python3-numpy', 'python3-uncertainties', # For AVO
     'python3-matplotlib', 'python3-scipy', 'python3-cairo', 'python3-cairocffi', 'vnc4server', 'fluxbox', 'python3-dev', 'python3-jsonpickle', # for da-graphing
-    'ghc', 'cabal-install', # for camfort
+    'ghc', 'cabal-install', 'liblapack3', # for camfort
     ]
   package { $jenkins_job_packages:
     ensure => installed,
