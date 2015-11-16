fcf0541fda279035488d416412ebf9c9c5df3426
Thomas Bytheway <tb403@cam.ac.uk>
2015-11-16 18:53:19 +0000

    Add gitlab ci runner config

---
+++ manifests/nodes/gitlab-runner.pp
@@ -0,0 +1,19 @@
+node /gitlab-runner(-\d+)?/ {
+  include 'dtg::minimal'
+  include 'docker'
+
+  package {
+    'apt-transport-https':
+    ensure => installed,
+  } ->
+  apt::source { 'gitlab-ci':
+    location => 'https://packages.gitlab.com/runner/gitlab-ci-multi-runner/ubuntu/',
+    repos => 'main',
+  } ->
+  package {
+    'gitlab-ci-multi-runner':
+    ensure => installed
+  }
+}
+
+
