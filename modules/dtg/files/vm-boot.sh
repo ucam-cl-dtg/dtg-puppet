65f222a2d1a3fc595736e2af9ff165996fe24ab0
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2015-11-16 23:08:55 +0000

    Don't add a partition to the data disk
    There's no point partitioning virtual disks. It just causes repartitioning pain. The filesystem can use all the disk.

--- modules/dtg/files/vm-boot.sh
+++ modules/dtg/files/vm-boot.sh
@@ -14,19 +14,18 @@ PUPPETBARE=/etc/puppet-bare
 BOOTSTRAP="https://raw.github.com/ucam-cl-dtg/dtg-puppet/master/modules/dtg/files/bootstrap.sh"
 AUTHORIZED_KEYS="/root/.ssh/authorized_keys"
 DOM0_PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAujx2sop6KNPYr6v/IWEpFITi964d89N3uVowvRo3X5f9a7fiquEphaXIvrMoF74TtmWe78NybfPPgKgTdmaBWYxhbykO7aC9QeY+iDcQqKWrLFlBAbqJ6GYJYfiSM0DZbmAXiAuguNhX1LU51zPRVKYf2/yAgCmJv2yammXppwCE+BJvBVqJziy2Cs0PKhI/26Altelc2tH+SMIlF9ZuSKCtAcyMTPQxTVrJ/zilmceh/U3LcLD3OlOD7XfHxUQ+fiH0KZ27dja6mnsb/OAvmqpmD8mYZs2vTUiFRH9V6HmQqQRO82a6XRRK6wHcGnh+J7JW45dO75lmtBElw1djyw== root@husky0.dtg.cl.cam.ac.uk"
-APT_TS=/var/lib/apt/periodic/update-success-stamp
+OBAPT_TS=/var/lib/apt/periodic/update-success-stamp
 # if there is a /dev/xvdb without partitions, let's use it
 
 mounted=`mount | grep /dev/xvdb`
 if [ -e /dev/xvdb ] && [ ! -e /dev/xvdb1 ] && [[ -z $mounted ]]; then
-    echo "Partitioning, and creating filesystem on /dev/xvdb"
-    (echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/xvdb
-    mkfs.ext4 /dev/xvdb1
-    echo "/dev/xvdb1 /local/data ext4 defaults,errors=remount-ro 0 1" >> /etc/fstab
+    echo "Creating a filesystem on /dev/xvdb"
+    mkfs.ext4 /dev/xvdb
+    echo "/dev/xvdb /local/data ext4 defaults,errors=remount-ro 0 1" >> /etc/fstab
     if [ ! -d /local/data ]; then
 	mkdir -p /local/data
     fi
-    mount /local/data
+    mount -a
 fi
 
 
