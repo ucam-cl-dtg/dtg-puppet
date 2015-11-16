15264b44ec27beecdc502f73e243905e3fdea47c
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2015-11-16 23:51:58 +0000

    Monitor logstash server

--- manifests/nodes/elk-logstash.pp
+++ manifests/nodes/elk-logstash.pp
@@ -3,3 +3,13 @@ node 'elk-logstash.dtg.cl.cam.ac.uk' {
   class { 'dtg::firewall::rsyslog': }
   class { 'dtg::elk::logs': }
 }
+
+
+if ( $::monitor ) {
+  nagios::monitor { 'elk-logstash':
+    parents    => 'nas04',
+    address    => 'elk-logstash.dtg.cl.cam.ac.uk',
+    hostgroups => [ 'ssh-servers' ],
+  }
+  munin::gatherer::configure_node { 'elk-logstash': }
+}
