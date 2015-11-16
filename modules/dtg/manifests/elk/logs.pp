d431c57519aaf7095c935955e48a0e0cdf57bfa1
Oliver Chick <oc243@cam.ac.uk>
2015-11-16 15:20:12 +0000

    Make logstash install correctly

--- modules/dtg/manifests/elk/logs.pp
+++ modules/dtg/manifests/elk/logs.pp
@@ -2,7 +2,7 @@ class dtg::elk::logs {
   class { 'dtg::apt_logstash': stage=> 'repos'}
   class { 'logstash':
     autoupgrade     => true,
-    install_contrib => true,
+    install_contrib => false,
     manage_repo     => false,
     java_install    => true,
 
@@ -22,6 +22,10 @@ class dtg::apt_logstash {
         location    => 'http://packages.elasticsearch.org/logstash/2.0/debian',
         release     => 'stable',
         repos       => 'main',
-        include     =>  {'src' => false}
+        include     =>  {'src' => false},
+        key         =>  {
+          'id'      => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
+          'source'  => 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch',
+        },
   }
 }
