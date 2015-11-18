f465c2687021a03a8a989f051c01f7aa43573d6c
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2015-11-18 22:49:44 +0000

    Monitor elk-elasticsearch

--- manifests/nodes/elk-elasticsearch.pp
+++ manifests/nodes/elk-elasticsearch.pp
@@ -7,3 +7,19 @@ node 'elk-elasticsearch.dtg.cl.cam.ac.uk' {
   class { 'dtg::firewall::publichttp': }
   class { 'dtg::firewall::80to8080': }
 }
+
+
+if ( $::monitor ) {
+  nagios::monitor { 'elk-elasticsearch':
+    parents    => 'nas04',
+    address    => 'elk-elasticsearch.dtg.cl.cam.ac.uk',
+    hostgroups => [ 'ssh-servers' ],
+  }
+  nagios::monitor { 'kibana':
+    parents                     => 'elk-elasticsearch',
+    address                     => 'kibana.dtg.cl.cam.ac.uk',
+    hostgroups                  => [ 'http-servers' ],
+    include_standard_hostgroups => false,
+  }
+  munin::gatherer::configure_node { 'elk-elasticsearch': }
+}
