64e26f2a18b77ed4d6cee83689a48da495771fa1
Oliver Chick <oc243@cam.ac.uk>
2015-11-18 09:53:40 +0000

    Add Kibana to elk stack

--- modules/dtg/manifests/elk/es.pp
+++ modules/dtg/manifests/elk/es.pp
@@ -1,16 +1,16 @@
 class dtg::elk::es {
 
   class { 'elasticsearch':
-    autoupgrade => true,
+    autoupgrade  => true,
     java_install => true,
-    datadir => '/local/data/elk-es',
-    manage_repo => false
+    datadir      => '/local/data/elk-es',
+    manage_repo  => false,
   }
 
   elasticsearch::instance { 'es-logs-0': }
 
   dtg::firewall::elasticsearch{'es-logs':
-    source => dnsLookup('logs.dtg.cl.cam.ac.uk'),
+    source      => dnsLookup('logs.dtg.cl.cam.ac.uk'),
     source_name => 'logstash',
   }
 
