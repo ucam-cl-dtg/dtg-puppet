b4c76efe268024e5f863bece5fd232c9206afe2f
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2015-11-20 14:52:06 +0000

    Monitor husky0's HTTPs certificate.
    So that we know when it is about to expire

--- manifests/nodes/husky.pp
+++ manifests/nodes/husky.pp
@@ -1,4 +1,9 @@
 if ( $::monitor ) {
+  nagios::monitor { 'husky0':
+    address    => 'husky0.dtg.cl.cam.ac.uk',
+    hostgroups => ['https-servers'],
+  }
+
   munin::gatherer::configure_node { 'husky0': }
   munin::gatherer::configure_node { 'husky1': }
   munin::gatherer::configure_node { 'husky2': }
