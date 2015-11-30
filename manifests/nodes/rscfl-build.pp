06a1a41e0afaa5e0e2148e51a3bd827f78673bb2
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2015-11-30 16:33:00 +0000

    Remove rscfl-build from monitoring.
    It's broken too

--- manifests/nodes/rscfl-build.pp
+++
@@ -1,8 +0,0 @@
-if ( $::monitor ) {
-  nagios::monitor { 'rscfl-build':
-    parents    => 'nas04',
-    address    => 'rscfl-build.dtg.cl.cam.ac.uk',
-    hostgroups => [ 'ssh-servers' ],
-  }
-  munin::gatherer::configure_node { 'rscfl-build': }
-}
