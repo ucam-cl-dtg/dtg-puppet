d81668a52b69694ca1fd3e5561c70d1e67c5a71e
Oliver Chick <oc243@cam.ac.uk>
2015-12-10 15:15:45 +0000

    Specify kibana version to get a more recent one than is default.
    This should be bumped  higher when we next upgrade Ubuntu as this is the last version of Kibana that supports Ubuntu's version of ES

--- manifests/nodes/elk-elasticsearch.pp
+++ manifests/nodes/elk-elasticsearch.pp
@@ -14,6 +14,7 @@ node 'elk-elasticsearch.dtg.cl.cam.ac.uk' {
   class {'kibana':
     port => 8080,
     default_app_id => 'dashboard/default-dashboard',
+    version => '4.1.3',
     # Override the default pid file location of /var/run/kibana.pid
     # as Kibana doesn't run as root and cannot create its pid
     # file due to permissions on /var/run
