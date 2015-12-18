085e5a37c31d37dcf31d1144ee6a73590c11e2bb
Sherif Akoush <sherif.akoush@gmail.com>
2015-12-18 16:34:26 +0000

    adding africa group on sa497-crunch-*

--- manifests/nodes/sa497-crunch.pp
+++ manifests/nodes/sa497-crunch.pp
@@ -5,8 +5,9 @@ node /sa497-crunch(-\d+)?/ {
 
     User<|title == sa497 |> { groups +>[ 'adm' ]}
 
-
-
+    dtg::sudoers_group{ 'africa':
+        group_name => 'africa',
+    }
 
     $packagelist = [ 'bison' , 'flex', 'autoconf' , 'pkg-config' , 'libglib2.0-dev', 'libpcap-dev' , 'liblz4-tool']
     package {
