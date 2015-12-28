54c5946b5a7cc91a88d4cbe796312202071ebcbb
Oliver Chick <oc243@cam.ac.uk>
2015-12-28 23:34:28 +0000

    Enable serial access to all physical machines

--- modules/dtg/manifests/baremetal.pp
+++ modules/dtg/manifests/baremetal.pp
@@ -2,4 +2,20 @@ class dtg::baremetal {
   package {'ipmitool':
     ensure  => installed,
   }
+
+  # Setup serial console.
+  file {'/etc/default/grub':
+    ensure => file,
+    source => 'puppet:///modules/dtg/grub',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  exec { "update-grub":
+    subscribe   => File["/etc/default/grub"],
+    refreshonly => true
+    }
+  
 }
+
