fa6a299ea4f0ab713d1d7da36fa079ec8986fc15
dac53 <dac53@cam.ac.uk>
2016-02-03 15:46:22 +0000

    Using auto.mnt to mount nas04. I assume this is it @Daniel?

--- manifests/nodes/dac53.pp
+++ manifests/nodes/dac53.pp
@@ -8,10 +8,21 @@ node 'dac53.dtg.cl.cam.ac.uk' {
     type   => 'ssh-rsa',
   }
   User<|title == dac53 |> { groups +>[ 'adm' ] }
-  
-  # mount nas04 on startup
-  file_line { 'mount nas04':
-    line => 'nas04.dtg.cl.cam.ac.uk:/dtg-pool0/deviceanalyzer /deviceanalyzerdata nfs defaults 0 0',
-    path => '/etc/fstab',
+
+ file {'/etc/auto.mnt':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => 'a=r',
+    content => 'nas04   nas04.dtg.cl.cam.ac.uk:/dtg-pool0/deviceanalyzer',
+  } ->
+  file_line {'mount nas':
+    line => '/mnt   /etc/auto.mnt',
+    path => '/etc/auto.master',
   }
+  file {'/nas4':
+    ensure => link,
+    target => '/mnt/nas04',
+  }
+  
 }
