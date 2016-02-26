2994b26ab61cc10c619bdda63a4061549b9de9dd
Andrew Rice <acr31@cam.ac.uk>
2016-02-26 15:57:39 +0000

    update sharenfs options if needed

--- modules/dtg/manifests/zfs.pp
+++ modules/dtg/manifests/zfs.pp
@@ -98,6 +98,11 @@ define dtg::zfs::fs ($pool_name, $fs_name, $share_opts, $compress_opts='on') {
     command => "sudo zfs create -o compression=${compress_opts} -o sharenfs=${share_opts} ${pool_name}/${fs_name}",
     onlyif  => "[  ! -d /${pool_name}/${fs_name} ]",
   }
+  ->
+  exec { "zfs set sharenfs ${pool_name}/${fs_name}":
+    command => "sudo zfs set sharenfs={$share_opts} ${pool_name}/${fs_name}",
+    onlyif => "[ `sudo zfs get -H -o value sharenfs ${pool_name}/${fs_name}` != \"${share_opts}\" ]"
+  }
 }
 
 class dtg::zfs::repos {
