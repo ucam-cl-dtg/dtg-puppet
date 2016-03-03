b9d709ecbe0bc5710c04943546cf3eeacadf87c8
Andrew Rice <acr31@cam.ac.uk>
2016-03-03 09:42:49 +0000

    fixed misplaced curly bracket

--- modules/dtg/manifests/zfs.pp
+++ modules/dtg/manifests/zfs.pp
@@ -100,7 +100,7 @@ define dtg::zfs::fs ($pool_name, $fs_name, $share_opts, $compress_opts='on') {
   }
   ->
   exec { "zfs set sharenfs ${pool_name}/${fs_name}":
-    command => "sudo zfs set sharenfs={$share_opts} ${pool_name}/${fs_name}",
+    command => "sudo zfs set sharenfs=${share_opts} ${pool_name}/${fs_name}",
     onlyif => "[ `sudo zfs get -H -o value sharenfs ${pool_name}/${fs_name}` != \"${share_opts}\" ]"
   }
 }
