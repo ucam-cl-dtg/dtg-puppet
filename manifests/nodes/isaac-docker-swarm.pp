3bb7ec7a40e684ffe6fba4681c4a58f11e39d4e6
Ian Davies <git@iandavies.org>
2016-04-04 13:08:15 +0100

    Add backup config to new Isaac server.

--- manifests/nodes/isaac-docker-swarm.pp
+++ manifests/nodes/isaac-docker-swarm.pp
@@ -6,10 +6,57 @@ node /isaac-\d+/ {
   class {'dtg::firewall::publichttp':}
   class {'dtg::firewall::publichttps':}
   
-  # dbbackup user
+  # User to own DB Backups
   user {'isaac':
     ensure => present,
     shell  => '/bin/bash',
     home   => '/usr/share/isaac'
   }
+
+  file { '/local/data/database-backup':
+    ensure => 'directory',
+    owner  => 'isaac',
+    group  => 'isaac',
+    mode   => '0755',
+  }
+  ->
+  file { '/local/data/database-backup/backups':
+    ensure => 'directory',
+    owner  => 'isaac',
+    group  => 'isaac',
+    mode   => '0755',
+  }
+  ->
+  file { '/local/data/database-backup/latest':
+    ensure => 'directory',
+    owner  => 'isaac',
+    group  => 'isaac',
+    mode   => '0755',
+  }
+  ->
+  file { '/local/data/isaac-docker-database-backup.sh':
+      mode   => '0755',
+      owner  => isaac,
+      group  => isaac,
+      source => 'puppet:///modules/dtg/isaac/isaac-docker-database-backup.sh'
+  }
+  ->
+  file { '/local/data/database-backup/isaac-database-backup.log':
+      path    => '/local/data/database-backup/isaac-database-backup.log',
+      ensure  => present,
+      replace => false,
+      mode    => '0755',
+      owner   => isaac,
+      group   => isaac,
+      content => '# Database backup log files'
+  }
+  ->
+  cron {'isaac-backup-database':
+    command => '/local/data/isaac-database-backup.sh >> /local/data/database-backup/isaac-database-backup.log',
+    user    => root,
+    hour    => 0,
+    minute  => 0
+  }
+
+
 }
