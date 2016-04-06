4bd10ed8363b8f214353cc757fc35144615a71b6
Andrew Rice <acr31@cam.ac.uk>
2016-04-06 11:29:12 +0100

    allowed users to override the name of the interfaces file that pre-up gets put into
    This is specifically for KVM guests on SmartOS which use an interfaces.d structure and so just sticking things in /etc/network/interfaces breaks all sorts of things

--- modules/dtg/manifests/firewall.pp
+++ modules/dtg/manifests/firewall.pp
@@ -14,7 +14,7 @@ class dtg::firewall::default {
     notify  => Exec['persist-firewall'],
   }
 }
-class dtg::firewall inherits dtg::firewall::default {
+class dtg::firewall inherits dtg::firewall::default($interface_file = '/etc/network/interfaces') {
   exec { 'persist-firewall':
     command     => '/sbin/iptables-save > /etc/iptables.rules',
     refreshonly => true,
@@ -22,7 +22,7 @@ class dtg::firewall inherits dtg::firewall::default {
   file_line { 'restore iptables':
     ensure => present,
     line   => 'pre-up iptables-restore < /etc/iptables.rules',
-    path   => '/etc/network/interfaces',
+    path   => $interface_file,
   }
   # Purge unmanaged firewall resources
   #
