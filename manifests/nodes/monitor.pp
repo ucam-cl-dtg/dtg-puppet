77ffeafcf2491c2d9a2ac9667eadb3d26802c88e
Daniel Thomas <drt24@cam.ac.uk>
2016-06-07 17:22:34 +0100

    We don't use epel and don't want to include the submodule

--- manifests/nodes/monitor.pp
+++ manifests/nodes/monitor.pp
@@ -4,7 +4,8 @@ node 'monitor.dtg.cl.cam.ac.uk' {
   apache::module {'headers':}
   # Use letsencrypt to get a certificate
   class {'letsencrypt':
-    email => $::from_address,
+    email          => $::from_address,
+    configure_epel => false,
   }
   $nagios_ssl = true
   class {'dtg::nagiosserver':}
