7b0d81633330661fa5e40fabb0e69c7b2f61cf6f
Andrew Rice <acr31@cam.ac.uk>
2016-06-10 12:18:34 +0100

    daily backup of dtg-pool0/backups/deviceanalyzer/archive

--- manifests/nodes/nas04.pp
+++ manifests/nodes/nas04.pp
@@ -163,6 +163,16 @@ node 'nas04.dtg.cl.cam.ac.uk' {
     comment          => 'Allow the backup user to use sudo for zfs',
   }
   ->
+  cron {"backup deviceanalyzer/archive":
+    ensure  => present,
+    user    => 'backup',
+    environment => "MAILTO=dtg-infra@cl.cam.ac.uk",
+    command => "nice -n 19 /bin/bash -c 'ssh root@malamute.dtg.cl.cam.ac.uk -i /home/backup/.ssh/id_rsa -o UserKnownHostsFile=/home/backup/.ssh/known_hosts zones/deviceanalyzer/archive@`sudo zfs list -H -t snapshot -d 1 -o name -S creation dtg-pool0/backups/deviceanalyzer/archive | head -n1 | cut --delim=\"@\" -f 2` | sudo zfs recv dtg-pool0/backups/deviceanalyzer/archive'",
+    minute  => cron_minute("backup deviceanalyzer/archive"),
+    hour    => cron_hour("backup deviceanalyzer/archive"),
+    weekday => "*",
+  }
+  
   dtg::zfs::fs{'backups/deviceanalyzer':
     pool_name  => $pool_name,
     fs_name    => 'backups/deviceanalyzer',
