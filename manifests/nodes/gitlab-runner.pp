43ecadc238d7582c044ee5a1f52002cd4f56d85d
Thomas Bytheway <tb403@cam.ac.uk>
2016-08-26 14:35:23 +0100

    Fix dependancy cycle in gitlab-runner config

--- manifests/nodes/gitlab-runner.pp
+++ manifests/nodes/gitlab-runner.pp
@@ -4,15 +4,18 @@ node /gitlab-runner(-\d+)?/ {
 
   class {'dtg::scm':}
 
-  apt::source { 'gitlab-ci':
-    location => 'https://packages.gitlab.com/runner/gitlab-ci-multi-runner/ubuntu/',
-    repos    => 'main',
-    require  => [ Package['apt-transport-https'] ],
-  } ->
+  class {'dtg::gitlab::runner::repos': stage => 'repos'}
+
   package {
     'gitlab-ci-multi-runner':
     ensure => installed
   }
 }
 
-
+class dtg::gilab::runner::repos{
+  apt::source { 'gitlab-ci':
+    location => 'https://packages.gitlab.com/runner/gitlab-ci-multi-runner/ubuntu/',
+    repos    => 'main',
+    require  => [ Package['apt-transport-https'] ],
+  }
+}
