6a5ae33f279ab61490e77f7bf6163406bbf99057
Daniel Thomas <drt24@cam.ac.uk>
2016-09-02 09:45:20 +0100

    Automatic puppet-lint fixes for modules/dtg

--- modules/dtg/manifests/zfs.pp
+++ modules/dtg/manifests/zfs.pp
@@ -99,6 +99,6 @@ define dtg::zfs::fs ($pool_name, $fs_name, $share_opts, $compress_opts='on') {
   ->
   exec { "zfs set sharenfs ${pool_name}/${fs_name}":
     command => "sudo zfs set sharenfs=${share_opts} ${pool_name}/${fs_name}",
-    onlyif => "[ `sudo zfs get -H -o value sharenfs ${pool_name}/${fs_name}` != \"${share_opts}\" ]"
+    onlyif  => "[ `sudo zfs get -H -o value sharenfs ${pool_name}/${fs_name}` != \"${share_opts}\" ]"
   }
 }
