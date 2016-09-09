0c4b1e014f9ec7c88b1912a67082ed5a765b5f05
Daniel Thomas <drt24@cam.ac.uk>
2016-09-09 13:28:30 +0100

    Add script and stage to test puppet config for all hostnames

---
+++ test-all-puppet.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Test our puppet config against all possible hosts
+
+set -e
+
+# Get the list of hostnames by querying the CL DNS and parsing the results
+domainNames=$(dig -t AXFR  cl.cam.ac.uk @dns0.cl.cam.ac.uk | grep .dtg.cl.cam.ac.uk | tr -s ' \t' ' ' | egrep '(CNAME)|(A)' | grep -v RRSIG | grep -v rscfl-vguest | cut -f 1,5 -d ' ' | tr ' ' '\n' | sort | uniq | grep -v '[0-9][0-9][0-9]' | grep -v puppy | grep -v so-22 | grep -v -- '-bmc' | grep dtg.cl.cam.ac.uk | cut -d '.' -f 1)
+
+function test-puppet() {
+  # This expects to be run inside the dtg-puppet repository e.g. ~/git/infrastructure/dtg-puppet
+  sudo -H FACTER_fqdn="${1}".dtg.cl.cam.ac.uk puppet apply --graph --noop --modulepath=modules --node_name_value="${1}".dtg.cl.cam.ac.uk manifests/nodes/
+}
+
+for host in $domainNames
+do
+  test-puppet "${host}"
+done
