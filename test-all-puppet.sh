a88e66742d2a713927ac35d0723a8a6e5a68938f
Daniel Thomas <drt24@cam.ac.uk>
2016-09-09 14:18:40 +0100

    Add cd to beginning of test-all-puppet

--- test-all-puppet.sh
+++ test-all-puppet.sh
@@ -4,6 +4,8 @@
 
 set -e
 
+cd "$(dirname "$0")"
+
 # Get the list of hostnames by querying the CL DNS and parsing the results
 domainNames=$(dig -t AXFR  cl.cam.ac.uk @dns0.cl.cam.ac.uk | grep .dtg.cl.cam.ac.uk | tr -s ' \t' ' ' | egrep '(CNAME)|(A)|(AAAA)' | grep -v RRSIG | grep -v rscfl-vguest | cut -f 1,5 -d ' ' | tr ' ' '\n' | sort | uniq | grep -v '[0-9][0-9][0-9]' | grep -v puppy | grep -v so-22 | grep -v -- '-bmc' | grep dtg.cl.cam.ac.uk | cut -d '.' -f 1)
 
