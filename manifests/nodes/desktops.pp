b0f9ecf8b21a4e10f0d2222dc6e208661cfd12d7
Daniel Thomas <drt24@cam.ac.uk>
2016-12-15 10:50:35 +0000

    Remove references to shin and pteria

--- manifests/nodes/desktops.pp
+++ manifests/nodes/desktops.pp
@@ -1,10 +1,6 @@
 $grapevine_ips = dnsLookup('grapevine.cl.cam.ac.uk')
 $grapevine_ip = $grapevine_ips[0]
-$shin_ips = dnsLookup('shin.cl.cam.ac.uk')
-$shin_ip = $shin_ips[0]
 $earlybird_ips = dnsLookup('earlybird.cl.cam.ac.uk')
 $earlybird_ip = $earlybird_ips[0]
-$pteria_ips = dnsLookup('pteria.cl.cam.ac.uk')
-$pteria_ip = $pteria_ips[0]
-$desktop_ips = "${grapevine_ip},${shin_ip},${earlybird_ip},${pteria_ip}"
-$desktop_ips_array = [$grapevine_ip, $shin_ip, $earlybird_ip, $pteria_ip]
+$desktop_ips = "${grapevine_ip},${earlybird_ip}"
+$desktop_ips_array = [$grapevine_ip, $earlybird_ip]
