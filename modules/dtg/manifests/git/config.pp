8b593f7817616933c15022afc6c704533cf6947c
Daniel Thomas <drt24@cam.ac.uk>
2017-01-18 15:14:14 +0000

    Configure git to always verify hashes so that corruptions can't spread

--- modules/dtg/manifests/git/config.pp
+++ modules/dtg/manifests/git/config.pp
@@ -3,6 +3,10 @@ class dtg::git::config {
     command => 'git config --system --add alias.graph \'log --graph --date-order -C -M --pretty=format:"<%h> %ad [%an] %Cgreen%d%Creset %s" --all --date=short\'',
     unless  => 'git config --get alias.graph',
   }
+  exec{'git transfer.fsckObjects':
+    command => 'git config --system --bool --add transfer.fsckObjects true',
+    unless  => 'git config --get transfer.fsckObjects',
+  }
   dtg::git::config::user{'root':
     real_name => 'DTG Infrastructure',
     email     => 'dtg-infra@cl.cam.ac.uk',
