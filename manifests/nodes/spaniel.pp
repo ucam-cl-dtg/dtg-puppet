c62e18aa73b951457a6f76a30b41592f1a0a9b3a
Daniel Thomas <drt24@cam.ac.uk>
2017-04-06 10:21:38 +0100

    Remove references to some non-existent hosts

--- manifests/nodes/spaniel.pp
+++
@@ -1,9 +0,0 @@
-node 'spaniel.dtg.cl.cam.ac.uk' {
-  # We don't have a local mirror of raspbian to point at
-  class { 'dtg::minimal': manageapt => false, }
-
-  $packages = ['xmonad', 'x11-xserver-utils', 'unclutter']
-  package{$packages:
-    ensure => installed
-  }
-}
