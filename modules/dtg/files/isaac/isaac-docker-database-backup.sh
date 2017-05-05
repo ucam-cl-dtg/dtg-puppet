663cff15e5b1fe66a94734d01c6ff96ef7822f90
James Sharkey <jps79@cam.ac.uk>
2017-05-05 10:21:20 +0100

    Fix line endings and other shell check warnings, update to match version on server
    # The version on the server had been edited to test silencing warning emails about mysql passwords
    # on the commnand line. This appears to have worked, and isn't too awful a solution; so persist the
    # change into puppet now.

--- modules/dtg/files/isaac/isaac-docker-database-backup.sh
+++ modules/dtg/files/isaac/isaac-docker-database-backup.sh
@@ -3,16 +3,16 @@
 # Requires 'backups' and 'latest' directories to be available
 # Dump the live DB, pipe into gzip, and place in local backup directory
 
-echo "Started isaac db backup at `date "+%Y-%m-%d %l:%M"`"
-ISAAC_BACKUP_FILE="/local/data/database-backup/backups/isaac-live-db-`date +%Y-%m-%d`.sql.gz"
+echo "Started isaac db backup at $(date "+%Y-%m-%d %l:%M")"
+ISAAC_BACKUP_FILE="/local/data/database-backup/backups/isaac-live-db-$(date +%Y-%m-%d).sql.gz"
 
-# Dump the Isaac DB
-/local/src/isaac-api/src/main/resources/db_scripts/dump-db.sh live | gzip > $ISAAC_BACKUP_FILE
-chown isaac:isaac $ISAAC_BACKUP_FILE
+# Dump the Isaac DB:
+/local/src/isaac-api/src/main/resources/db_scripts/dump-db.sh live | gzip > "$ISAAC_BACKUP_FILE"
+chown isaac:isaac "$ISAAC_BACKUP_FILE"
 
-# Copy the latest
+# Copy the latest:
 rm /local/data/database-backup/latest/isaac-live-db-*
-cp $ISAAC_BACKUP_FILE /local/data/database-backup/latest
+cp "$ISAAC_BACKUP_FILE" /local/data/database-backup/latest
 
-# Removes backups that are older than 10 days
+# Removes backups that are older than 10 days:
 find "/local/data/database-backup/backups" -type f -prune -mtime +10 -exec rm -f {} \;
