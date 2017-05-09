0396c394f47dd2c344564e5fb5647499898d2fda
Thomas Bytheway <tb403@cam.ac.uk>
2017-05-09 11:19:44 +0100

    Shift keys to root users home directory

--- modules/dtg/manifests/puppy_deploy_key.pp
+++ modules/dtg/manifests/puppy_deploy_key.pp
@@ -6,20 +6,28 @@ class dtg::puppy_deploy_key {
   # be publicly read anyway. The deploy key on gitlab is
   # appropriately flagged with a bold warning that the key is
   # publicly accessible.
-  file {'/etc/ssh/puppy-deploy-public':
-    ensure => file,
+  file {'/root/.ssh':
+    ensure => directory,
     owner  => 'root',
-    group  => 'adm',
     mode   => '0600',
-    source => 'puppet:///modules/dtg/files/ssh/puppy-deploy-public',
   }
 
-  file {'/etc/ssh/puppy-deploy-public.pub':
-    ensure => file,
-    owner  => 'root',
-    group  => 'adm',
-    mode   => '0644',
-    source => 'puppet:///modules/dtg/files/ssh/puppy-deploy-public.pub',
+  file {'/root/.ssh/puppy-deploy-public':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'adm',
+    mode    => '0600',
+    source  => 'puppet:///modules/dtg/files/ssh/puppy-deploy-public',
+    require => [ File['/root/.ssh'] ],
+  }
+
+  file {'/root/.ssh/puppy-deploy-public.pub':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'adm',
+    mode    => '0644',
+    source  => 'puppet:///modules/dtg/files/ssh/puppy-deploy-public.pub',
+    require => [ File['/root/.ssh'] ],
   }
 
   ::ssh::client::config::user { 'root':
@@ -28,9 +36,10 @@ class dtg::puppy_deploy_key {
     manage_user_ssh_dir => false,
     options             => {
       'Host gitlab.dtg.cl.cam.ac.uk' => {
-        'IdentityFile' => '/etc/ssh/puppy-deploy-public',
+        'IdentityFile' => '/root/.ssh/puppy-deploy-public',
       },
     },
+    require             => [ File['/root/.ssh/puppy-deploy-public']],
   }
 }
 
