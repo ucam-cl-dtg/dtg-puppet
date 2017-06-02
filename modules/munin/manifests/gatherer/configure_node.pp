1eea816e70a7a2fcae9a2b8e73e3a403aea293af
Daniel Thomas <drt24@cam.ac.uk>
2017-06-02 17:41:08 +0100

    Allow specifying a custom address for munin gathering

--- modules/munin/manifests/gatherer/configure_node.pp
+++ modules/munin/manifests/gatherer/configure_node.pp
@@ -1,5 +1,10 @@
-define munin::gatherer::configure_node ( $override_lines = '') {
+define munin::gatherer::configure_node ( $override_lines = '', $address = '') {
   $munin_node_host = $title
+  if $address == '' {
+    $munin_node_address = "${munin_node_host}.${::org_domain}"
+  } else {
+    $munin_node_address = $address
+  }
   file { "/etc/munin/munin-conf.d/${munin_node_host}":
     ensure  => present,
     content => template('munin/node.erb'),
