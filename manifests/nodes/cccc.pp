a8bf16132adf817491030f86bfff2089db921d69
Daniel Thomas <drt24@cam.ac.uk>
2017-06-02 17:41:37 +0100

    monitor shalmaneser4 with munin

--- manifests/nodes/cccc.pp
+++ manifests/nodes/cccc.pp
@@ -4,6 +4,9 @@ if ( $::monitor ) {
     address    => 'shalmaneser3.sec.cl.cam.ac.uk',
     hostgroups => ['ssh-servers'],
   }
+  munin::gatherer::configure_node { 'shalmaneser4':
+    address => 'shalmaneser4.sec.cl.cam.ac.uk',
+  }
   nagios::monitor { 'shalmaneser4':
     parents    => '',
     address    => 'shalmaneser4.sec.cl.cam.ac.uk',
