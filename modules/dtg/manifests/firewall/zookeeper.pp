841254a1521b207839d927d151a1bce72c65a7f6
Ian Davies <ipd21@cam.ac.uk>
2017-07-17 13:43:12 +0100

    Add zookeeper firewall config

---
+++ modules/dtg/manifests/firewall/zookeeper.pp
@@ -0,0 +1,12 @@
+class dtg::firewall::zookeeper inherits dtg::firewall::default {
+  firewall { '010 accept all zookeeper traffic':
+    proto  => 'tcp',
+    dport  => '2181',
+    action => 'accept',
+  }
+  firewall { '011 accept all zookeeper election traffic':
+    proto  => 'tcp',
+    dport  => '3181',
+    action => 'accept',
+  }
+}
