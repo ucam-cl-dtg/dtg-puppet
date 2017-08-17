0d14b70c5b39c73799bc6d92bff5a7e677a5d884
Daniel Thomas <drt24@cam.ac.uk>
2017-08-17 15:39:16 +0100

    Remove reference to machine that no longer exists

--- manifests/nodes/nas04.pp
+++ manifests/nodes/nas04.pp
@@ -10,7 +10,6 @@ node 'nas04.dtg.cl.cam.ac.uk' {
                   'dac53.dtg.cl.cam.ac.uk',
                   'grapevine.cl.cam.ac.uk',
                   'earlybird.cl.cam.ac.uk',
-                  'deviceanalyzer-visitor-jk672.dtg.cl.cam.ac.uk',
                   'sak70-math.dtg.cl.cam.ac.uk']
 
   # bonded nics
