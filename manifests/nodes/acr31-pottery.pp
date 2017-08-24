e12c85f70c837b92cd2d43ba80a9d342d8a40633
Andrew Rice <acr31@cam.ac.uk>
2017-08-24 16:24:38 +0100

    Updated config for acr31-pottery

--- manifests/nodes/acr31-pottery.pp
+++ manifests/nodes/acr31-pottery.pp
@@ -3,31 +3,31 @@ node /^acr31-pottery/ {
 
   class {'dtg::pottery::aptsources': stage => 'repos' }
   
-  file{'/local/data/docker':
-    ensure => directory,
-    owner  => 'root'
-  }
-  ->
-  package{'docker-ce':
-    ensure => installed,
-  }
-  ->
-  class { 'docker':
-    tcp_bind                    => 'tcp://127.0.0.1:2375',
-    socket_bind                 => 'unix:///var/run/docker.sock',
-    root_dir                    => '/local/data/docker',
-    use_upstream_package_source => false,
-    manage_package              => false,
-  }
-  ->
-  exec {'add-www-data-to-docker-group':
-    unless  => "/bin/grep -q '^docker:\\S*www-data' /etc/group",
-    command => '/usr/sbin/usermod -aG docker www-data',
-  }
-  ->
-  docker::image { 'ubuntu':
-    image_tag => '16.04'
-  }
+  # file{'/local/data/docker':
+  #   ensure => directory,
+  #   owner  => 'root'
+  # }
+  # ->
+  # package{'docker-ce':
+  #   ensure => installed,
+  # }
+  # ->
+  # class { 'docker':
+  #   tcp_bind                    => 'tcp://127.0.0.1:2375',
+  #   socket_bind                 => 'unix:///var/run/docker.sock',
+  #   root_dir                    => '/local/data/docker',
+  #   use_upstream_package_source => false,
+  #   manage_package              => false,
+  # }
+  # ->
+  # exec {'add-www-data-to-docker-group':
+  #   unless  => "/bin/grep -q '^docker:\\S*www-data' /etc/group",
+  #   command => '/usr/sbin/usermod -aG docker www-data',
+  # }
+  # ->
+  # docker::image { 'ubuntu':
+  #   image_tag => '16.04'
+  # }
     
   class {'dtg::firewall::publichttps':}
   ->
@@ -51,6 +51,28 @@ node /^acr31-pottery/ {
   ->
   apache::site {'pottery':
     source => 'puppet:///modules/dtg/apache/pottery.conf',
+  }
+  ->
+  exec {'generate-AAHeaderKey':
+    command => '/bin/bash -c "echo -n \"AAHeaderKey \"; openssl rand -hex 24" > /etc/apache2/AAHeaderKey.conf',
+    creates => '/etc/apache2/AAHeaderKey.conf'
+  }
+  ->
+  file {'/etc/apache2/AAHeaderKey.conf':
+    owner => 'root',
+    group => 'root',
+    mode => '600'
+  }
+  ->
+  exec {'generate-AACookieKey':
+    command => '/bin/bash -c "echo -n \"AACookieKey \"; openssl rand -hex 24" > /etc/apache2/AACookieKey.conf',
+    creates => '/etc/apache2/AACookieKey.conf'
+  }
+  ->
+  file {'/etc/apache2/AACookieKey.conf':
+    owner => 'root',
+    group => 'root',
+    mode => '600'
   }
   
   class { 'postgresql::globals':
