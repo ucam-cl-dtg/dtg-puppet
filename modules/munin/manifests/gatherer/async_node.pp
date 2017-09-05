04a2732af3669ca1fe387fa5ae54b227207e175c
Daniel Thomas <drt24@cam.ac.uk>
2017-09-05 17:40:18 +0100

    Support manually setting the full hostname for munin async nodes

--- modules/munin/manifests/gatherer/async_node.pp
+++ modules/munin/manifests/gatherer/async_node.pp
@@ -1,5 +1,10 @@
-define munin::gatherer::async_node ( $override_lines = '') {
+define munin::gatherer::async_node ( $override_lines = '', $full_hostname = undef) {
   $munin_node_host = $title
+  if ($full_hostname) {
+    $munin_node_full_hostname = $full_hostname
+  } else {
+    $munin_node_full_hostname = "${munin_node_host}.${::org_domain}"
+  }
   file { "/etc/munin/munin-conf.d/${munin_node_host}":
     ensure  => present,
     content => template('munin/node-async.erb'),
