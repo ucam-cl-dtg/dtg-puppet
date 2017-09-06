8a94bc540e30833212a213d5a470f585d78a37fb
Daniel Thomas <drt24@cam.ac.uk>
2017-09-06 15:54:20 +0100

    Add Richard's user and key

--- manifests/nodes/faucet.pp
+++ manifests/nodes/faucet.pp
@@ -5,6 +5,18 @@ node 'faucet.sec.cl.cam.ac.uk' {
   class { 'dtg::minimal':
     user_whitelist => ['drt24', 'rnc1', 'rss39']
   }
+
+  dtg::add_user { 'rnc1':
+    real_name => 'Richard Clayton',
+    groups    => 'adm',
+    uid       => '1738',
+  } ->
+  ssh_authorized_key {'rnc1':
+    ensure => present,
+    key    => 'AAAAB3NzaC1yc2EAAAABIwAAAIEAqKRiv2o4l9zOtNSyjS1kTqKK0r/4+z8VRhVQddCq+p93m19SwuA2kHDLZMy3fJRhZwuCE+F2fRNiX320/tgXjPM431mwVqZo2VcJXmZmn2HRwA+Iiakckqdc244qv/H0vlRGoPM1m156kZvKYAEa8y4pJq4azJMj+IGFf+n/+rs=',
+    type   => 'ssh-rsa',
+    user   => 'rnc1',
+  }
   
 
   package{$packages:
