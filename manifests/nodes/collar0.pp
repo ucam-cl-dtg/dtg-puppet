1cfd51a9779dd97c126fd6f490e97782f47e4b5b
Daniel Thomas <drt24@cam.ac.uk>
2018-01-08 12:18:08 +0000

    Remove collar0 and 1 from config as unused

--- manifests/nodes/collar0.pp
+++
@@ -1,7 +0,0 @@
-if ( $::monitor ) {
-  nagios::monitor { 'collar0':
-    parents    => '',
-    address    => 'collar0.dtg.cl.cam.ac.uk',
-    hostgroups => [ 'http-servers' ],
-  }
-}
--- manifests/nodes/collar1.pp
+++
@@ -1,7 +0,0 @@
-if ( $::monitor ) {
-  nagios::monitor { 'collar1':
-    parents    => '',
-    address    => 'collar1.dtg.cl.cam.ac.uk',
-    hostgroups => [ 'http-servers' ],
-  }
-}
