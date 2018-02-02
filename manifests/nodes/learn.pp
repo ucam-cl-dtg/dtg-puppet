d04ba900978da14e304a568fb71a5435bffb8c89
Andrew Rice <acr31@cam.ac.uk>
2018-02-02 16:13:19 +0000

    removed learn and minibus which are now retired

--- manifests/nodes/learn.pp
+++
@@ -1,8 +0,0 @@
-if ( $::monitor ) {
-  nagios::monitor { 'learn':
-    parents    => 'nas04',
-    address    => 'learn.dtg.cl.cam.ac.uk',
-    hostgroups => [ 'ssh-servers', 'http-servers' ],
-  }
-  munin::gatherer::async_node { 'learn': }
-}
--- manifests/nodes/minibus.pp
+++
@@ -1,8 +0,0 @@
-if ( $::monitor ) {
-  nagios::monitor { 'minibus':
-    parents    => 'nas04',
-    address    => 'minibus.dtg.cl.cam.ac.uk',
-    hostgroups => [ 'ssh-servers', 'http-servers' ],
-  }
-  munin::gatherer::async_node { 'minibus': }
-}
