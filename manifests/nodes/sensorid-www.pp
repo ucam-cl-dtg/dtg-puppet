007c7c1f6aa0d61525d37a8205e650665c698e51
jz448 <jz448@cam.ac.uk>
2018-02-08 15:28:27 +0000

    Update sensorid-www.pp

--- manifests/nodes/sensorid-www.pp
+++ manifests/nodes/sensorid-www.pp
@@ -4,8 +4,6 @@ node 'sensorid-www.dtg.cl.cam.ac.uk' {
   include 'dtg::minimal'
 
   class {'dtg::firewall::privatehttp':}
-  class {'dtg::firewall::privatehttps':}
-  class {'dtg::firewall::80to5000':}
   
   # Packages which should be installed
   $packagelist = ['nginx']
