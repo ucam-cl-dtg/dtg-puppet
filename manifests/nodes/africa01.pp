01f74a1e9104e8d7d2b8df3f15c5ab23c7a65b62
Daniel Thomas <drt24@cam.ac.uk>
2018-02-26 11:55:04 +0000

    Tune config of postgresql on africa01

--- manifests/nodes/africa01.pp
+++ manifests/nodes/africa01.pp
@@ -186,7 +186,31 @@ node 'africa01.dtg.cl.cam.ac.uk' {
     listen_addresses           => '*',
     ipv4acls                   => ['local all all peer'],
   }
-  ->
+  -> # Performance tuning for higher memory usage
+  postgresql::server::config_entry{'max_wal_size':
+    ensure => present,
+    value  => '10G',
+  } ->
+  postgresql::server::config_entry{'shared_buffers':
+    ensure => present,
+    value  => '1GB',
+  } ->
+  postgresql::server::config_entry{'temp_buffers':
+    ensure => present,
+    value  => '64MB',
+  } ->
+  postgresql::server::config_entry{'work_mem':
+    ensure => present,
+    value  => '1GB',
+  } ->
+  postgresql::server::config_entry{'maintenance_work_mem':
+    ensure => present,
+    value  => '2GB',
+  } ->
+  postgresql::server::config_entry{'effective_io_concurrency':
+    ensure => present,
+    value  => '4',
+  } ->
   postgresql::server::tablespace{'cccc':
     location => "/${pool_name}/cccc/postgresql/",
     require  => Dtg::Zfs::Fs['cccc/postgresql']
