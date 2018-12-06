b5a70ff67c8667faf57a12043c8adea3e5adb0e7
Daniel Thomas <daniel@danielkirsty.me.uk>
2018-12-06 11:52:40 +0000

    Add monitoring of shalmaneser{5,6}

--- manifests/nodes/cccc.pp
+++ manifests/nodes/cccc.pp
@@ -15,5 +15,26 @@ if ( $::monitor ) {
     address    => 'shalmaneser4.sec.cl.cam.ac.uk',
     hostgroups => ['ssh-servers'],
   }
+  munin::gatherer::configure_node { 'shalmaneser5':
+    address => 'shalmaneser5.sec.cl.cam.ac.uk',
+  }
+  nagios::monitor { 'shalmaneser5':
+    parents    => '',
+    address    => 'shalmaneser5.sec.cl.cam.ac.uk',
+    hostgroups => ['ssh-servers', 'https-servers'],
+  } ->
+  nagios::monitor { 'data.cambridgecybercrime.uk':
+    parents    => 'shalmaneser5',
+    address    => 'data.cambridgecybercrime.uk',
+    hostgroups => ['https-servers'],
+  }
+  munin::gatherer::configure_node { 'shalmaneser6':
+    address => 'shalmaneser6.sec.cl.cam.ac.uk',
+  }
+  nagios::monitor { 'shalmaneser6':
+    parents    => '',
+    address    => 'shalmaneser6.sec.cl.cam.ac.uk',
+    hostgroups => ['ssh-servers'],
+  }
 }
 
