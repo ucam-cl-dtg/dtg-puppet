68a8df3d1a0f0282b138e249eb486fcb311f1645
Daniel Thomas <drt24@cam.ac.uk>
2019-02-07 16:51:14 +0000

    Properly strip out some left in pound stuff

--- manifests/nodes/cccc-vh315-darkmarkets.pp
+++ manifests/nodes/cccc-vh315-darkmarkets.pp
@@ -25,18 +25,11 @@ node 'cccc-vh315-darkmarkets.dtg.cl.cam.ac.uk' {
     email          => $::from_address,
     configure_epel => false,
     require        => [Service['apache2'], Class['dtg::firewall::publichttp']],
-  } ->
-  exec {'letsencrypt first run without working pound':
-    command => "letsencrypt --agree-tos certonly -a standalone -d ${::fqdn} && cat /etc/letsencrypt/live/${::fqdn}/privkey.pem /etc/letsencrypt/live/${::fqdn}/fullchain.pem > /etc/letsencrypt/live/${::fqdn}/privkey_fullchain.pem && service pound restart",
-    unless  => 'lsof -i TCP | grep pound | grep \'*:http\'',
-    require => Package['lsof'],
   }
   letsencrypt::certonly { $::fqdn:
     plugin          => 'webroot',
     webroot_paths   => ['/var/www/'],
     manage_cron     => true,
-    # Evil hack because pound requires everything in the same file
-    additional_args => [" && cat /etc/letsencrypt/live/${::fqdn}/privkey.pem /etc/letsencrypt/live/${::fqdn}/fullchain.pem > /etc/letsencrypt/live/${::fqdn}/privkey_fullchain.pem"],
     require         => Class['letsencrypt']
   }
 
