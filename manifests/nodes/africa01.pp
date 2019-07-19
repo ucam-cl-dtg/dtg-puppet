d97ccbc842a2305d59e05b2caa66a79043ffb450
Daniel Thomas <drt24@cam.ac.uk>
2019-07-19 16:01:28 +0100

    Give Richard access to africa01

--- manifests/nodes/africa01.pp
+++ manifests/nodes/africa01.pp
@@ -2,6 +2,18 @@ node 'africa01.dtg.cl.cam.ac.uk' {
   include 'dtg::minimal'
   include 'dtg::nfs'
 
+  dtg::add_user { 'rnc1':
+    real_name => 'Richard Clayton',
+    groups    => 'adm',
+    uid       => '1738',
+  } ->
+  ssh_authorized_key {'rnc1':
+    ensure => present,
+    key    => 'AAAAB3NzaC1yc2EAAAABIwAAAIEAqKRiv2o4l9zOtNSyjS1kTqKK0r/4+z8VRhVQddCq+p93m19SwuA2kHDLZMy3fJRhZwuCE+F2fRNiX320/tgXjPM431mwVqZo2VcJXmZmn2HRwA+Iiakckqdc244qv/H0vlRGoPM1m156kZvKYAEa8y4pJq4azJMj+IGFf+n/+rs=',
+    type   => 'ssh-rsa',
+    user   => 'rnc1',
+  }
+
   class { 'dtg::bonding': address => '128.232.23.175'}
 
   $pool_name = 'data-pool0'
